27c444422325198eab608718e9e4a8c85fa8deef
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2018-09-05 00:23:50 -0400

    intermediate revision - temporarily disabled some unresolved annotations, upgraded to spring-boot-starter to 1.5.4.RELEASE

--- springboot_javafx/README.md
+++ springboot_javafx/README.md
@@ -13,8 +13,8 @@ mvn clean spring-boot:run
 ### See also
 
 * [SpringBoot + JavaFx publication](https://habrahabr.ru/post/265511/) (in russian)
-*  [javaFx spring day 1](https://dzone.com/articles/javafx-spring-day-1-–)
-*  [javaFx Spring ay 3](https://dzone.com/articles/javafx-spring-day-3-–)
-*  [javafx2 with spring](http://koenserneels.blogspot.com/2012/11/javafx-2-with-spring.html)
+* [javaFx spring day 1](https://dzone.com/articles/javafx-spring-day-1-–)
+* [javaFx Spring ay 3](https://dzone.com/articles/javafx-spring-day-3-–)
+* [javafx2 with spring](http://koenserneels.blogspot.com/2012/11/javafx-2-with-spring.html)
 
 
--- springboot_javafx/src/test/java/ru/habrahabr/ApplicationTests.java
+++ springboot_javafx/src/test/java/ru/habrahabr/ApplicationTests.java
@@ -4,11 +4,14 @@ import javafx.embed.swing.JFXPanel;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+// NOTE: compilation problems when upgraded the org.springframework.boot.spring-boot-starter-parent
+// from 1.2.5.RELEASE to 1.5.4.RELEASE
+// package org.springframework.boot.test does not exist
+// import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+// @SpringApplicationConfiguration(classes = Application.class)
 public class ApplicationTests {
 
 	@BeforeClass
