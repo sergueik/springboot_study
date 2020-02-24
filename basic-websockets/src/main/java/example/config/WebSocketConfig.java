59a1bcb4bbdd2e320da6d643b271cf9526a97f5a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-02-24 03:33:46 +0100

    initial snapshot, keeping with sbs 2.x

---
+++ basic-websockets/src/main/java/example/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	// see also https://www.baeldung.com/spring-websockets-sendtouser
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+		registry.enableSimpleBroker("/topic"); // Enables a simple in-memory broker
+
+	}
+}
