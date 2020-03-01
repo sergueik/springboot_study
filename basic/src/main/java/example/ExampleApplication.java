16ed1852ec57306ae8849d06bb51ffbaf9b44cee
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-02 00:47:41 +0100

    intermediate revision - added skeleton unit tests

--- basic/src/main/java/example/ExampleApplication.java
+++ basic/src/main/java/example/ExampleApplication.java
@@ -3,14 +3,23 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO: does not see its own project tree (does not need to with Spring though)
-// import example.resource.BasicResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 // NOTE: SpringBootApplication cannot be default package
+
 @SpringBootApplication
+@RestController
+@RequestMapping("/basic")
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello basic";
 	}
 }
