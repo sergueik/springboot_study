e01f70df4a0e9d9966ed10a643c7302791640012
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-03-21 15:27:11 -0400

    intermediate revision - added debug parameter  autoloading through @value annotation, minor refactoring

--- basic-static/README.md
+++ basic-static/README.md
@@ -1,1 +1,22 @@
-# https://github.com/deekshasharma/springboot-web-app-static
+### Info
+
+Basic Springboot application hosting static page with a Angular JS  applcation, with unit test on HTTP status and page content validations added
+
+### Testing
+```sh
+mvn  -Ddebug=true test
+```
+`debug` flag will trigger printig additional debugging information
+
+### Running
+```sh
+mvn clean -Dmaven.test.skip=true spring-boot:run
+```
+followed by
+
+```sh
+curl http://localhost:8080
+```
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
--- basic-static/src/test/java/example/LauncherTest.java
+++ basic-static/src/test/java/example/LauncherTest.java
@@ -1,68 +1,53 @@
 package example;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import static org.hamcrest.CoreMatchers.is;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.mockito.Matchers.any;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import org.owasp.validator.css.CssScanner;
-import org.owasp.validator.css.ExternalCssScanner;
-import org.owasp.validator.html.CleanResults;
-import org.owasp.validator.html.InternalPolicy;
-import org.owasp.validator.html.Policy;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Locale;
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-import java.util.stream.Stream;
 
-import javax.servlet.ServletOutputStream;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import org.owasp.validator.css.CssScanner;
+import org.owasp.validator.html.CleanResults;
+import org.owasp.validator.html.InternalPolicy;
 import org.owasp.validator.html.PolicyException;
-import org.xml.sax.SAXParseException;
-// see also:
-// https://github.com/andresriancho/owaspantisamy/blob/master/Java/antisamy/src/main/java/org/owasp/validator/css/CssScanner.java
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(Launcher.class)
 public class LauncherTest {
+
+	private static final String htmlPath = "/index.html";
+	private static final String cssPath = "/css/main.css";
+	private static final String scriptPath = "/js/script.js";
+	private static final String resourcePath = "src/main/resources/static";
+	// private static final boolean debug = false;
+
+	@Value("${debug:false}")
+	boolean debug;
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	public void statusTest() throws java.lang.Exception {
-		final String resourcePath = "/js/script.js";
 		// Act
-		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(resourcePath)
+		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(scriptPath)
 				.accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
 		// Assert
 		assertEquals("Incorrect Response Status", HttpStatus.OK.value(),
@@ -70,9 +55,8 @@ public class LauncherTest {
 	}
 
 	@Test
+	// https://www.programcreek.com/java-api-examples/?code=werval/werval/werval-master/io.werval.modules/io.werval.modules.sanitize/src/main/java/io/werval/modules/sanitize/Sanitize.java
 	public void cssTest() throws java.lang.Exception {
-		final String htmlPath = "/index.html";
-		final String cssPath = "/css/main.css";
 		// Act
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(cssPath)
 				.accept(MediaType.APPLICATION_JSON_UTF8)).andReturn();
@@ -80,26 +64,28 @@ public class LauncherTest {
 		assertEquals("Incorrect Response Status", HttpStatus.OK.value(),
 				result.getResponse().getStatus());
 		String input = result.getResponse().getContentAsString();
-		// https://www.programcreek.com/java-api-examples/?code=werval/werval/werval-master/io.werval.modules/io.werval.modules.sanitize/src/main/java/io/werval/modules/sanitize/Sanitize.java
-		System.err.println("Validating: " + input);
-		final ClassLoader loader = this.getClass().getClassLoader();
+		if (debug) {
+			System.err.println("Validating: " + input);
+		}
 		ResourceBundle messages = ResourceBundle.getBundle("AntiSamy", Locale.US,
-				loader);
+				this.getClass().getClassLoader());
 		CssScanner cssScanner = null;
 		try {
 			cssScanner = new CssScanner(
 					(InternalPolicy) InternalPolicy.getInstance(getPageContent(htmlPath)),
 					messages);
 		} catch (PolicyException e) {
-			// org.xml.sax.SAXParseException;
-			// static/css/main.css; lineNumber: 1; columnNumber: 1; Content is not
-			// allowed in prolog.
-			// is attempting to scan CSS as HTML
+			// org.xml.sax.SAXParseException Content is not allowed in prolog.
+			// when attempting to initialize InternalPolicy with css instead of html
 		}
 		if (cssScanner != null) {
 			CleanResults results = cssScanner.scanStyleSheet(input,
 					Integer.MAX_VALUE);
-			assertEquals("Invalid CSS", 0, results.getNumberOfErrors());
+			assertEquals(String.format("Invalid CSS in %s", cssPath), 0,
+					results.getNumberOfErrors());
+		}
+		if (debug) {
+			System.err.println("Debug: " + debug);
 		}
 	}
 
@@ -107,16 +93,18 @@ public class LauncherTest {
 	protected String getPageContent(String pagename) {
 		try {
 			URI uri = this.getClass().getClassLoader().getResource(pagename).toURI();
-			System.err.println("Testing local file: " + uri.toString());
+			if (debug) {
+				System.err.println("Testing local file: " + uri.toString());
+			}
 			return uri.toString();
 		} catch (URISyntaxException | NullPointerException e) {
 			// mask the exception when debug
-			final String mockupPath = String.format("src/main/resources/static/%s",
-					pagename);
-			System.err.println("Testing mocked up file: " + mockupPath);
-			return mockupPath;
+			if (debug) {
+				System.err
+						.println("Testing mocked up file: " + resourcePath + pagename);
+			}
+			return resourcePath + pagename;
 			// throw new RuntimeException(e);
-
 		}
 	}
 
