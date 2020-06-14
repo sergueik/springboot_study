aa0596c9ce55be17002e0f28b6d4b3938f4a344d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-06-14 03:03:39 +0200

    intermediate revision - refactoring and documenting what is going on

--- basic-mysql/syntax_check.sh
+++ basic-mysql/syntax_check.sh
@@ -1,16 +1,24 @@
-#!/bin/sh 
-# bad substitution in regular shell
+#!/bin/bash
 # works fine with bash and ash (alpine)
-MSG='port 123'
+# bad substitution error indicates that the regular shell is processing
+# see also: https://www.cyberciti.biz/tips/bash-shell-parameter-substitution-2.html
+MSG='port 3360'
 if [[ "${MSG//[^a-z ]/}" == 'port ' ]]; then
-  echo "PORT = ${MSG//[^0-9]/}"; 
+  PORT=${MSG//[^0-9]/}
 else
-  echo 'Failed to use bash syntax'
+  echo 'Failed to use bash syntax, using default port'
+  PORT=3306
 fi
-MSG='host mysql_db'
-if [[ "${MSG// [a-z_]*/}" == 'host' ]]; then
-  echo "HOST = ${MSG//host /}"; 
+echo "PORT = ${PORT}";
+
+MSG='host mysql_host'
+# NOTE: ${MSG// [a-z_]/} would fail
+if [[ "${MSG/ [a-z_]*/}" == 'host' ]]; then
+  HOST=${MSG//host /}
 else
   echo 'Failed to use bash syntax extensions'
+  HOST='localhost'
 fi
+
+echo "HOST = ${HOST}";
 
