b3a4b6bd2ae38b050308efa2efc0591a0892c413
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-08-01 21:44:57 -0400

    intermediate revision - converted to springboot project and minor configuration changes to loggers

--- basic-logback/README.md
+++ basic-logback/README.md
@@ -1,6 +1,6 @@
 ### Info
 
-This project contains [minimal demo code of logback example](http://logback.qos.ch/manual/appenders.html)
+This project contains [minimal demo code of logback example](http://logback.qos.ch/manual/appenders.html) converted to a regular springboot application logging initialization and operation
 
 ### Usage
 
@@ -11,16 +11,35 @@ java -cp target\logback-0.0.1-SNAPSHOT.jar;target\lib\*;target\conf example.Exam
 ```
 Note: it does not appear that logback's `RollingFileAppender` [class](https://github.com/qos-ch/logback/blob/master/logback-core/src/main/java/ch/qos/logback/core/rolling/RollingFileAppender.java)
 supports configuring log file permissions.
-
+### Testing Springboot App 
+```sh
+mvn spring-boot:run
+```
+and check the messages in `App.log` and console:
+```sh
+21:35:06.166 [main] INFO  o.s.b.c.e.t.TomcatEmbeddedServletContainer - Tomcat st
+arted on port(s): 8080 (http)
+```
+and
+```sh
+3175 [main] INFO  example.Example - init message
+3175 [main] WARN  example.Example - init message
+```
+in `App.log` only
+then
+```sh
+curl http://localhost:8080/example?data=42
+```
+and check the appearance of new messages in `App.log`:
+```sh
+41629 [http-nio-8080-exec-1] INFO  example.Example - exampleHandler received: 42
+41629 [http-nio-8080-exec-1] WARN  example.Example - exampleHandler received: 42
+41629 [http-nio-8080-exec-1] DEBUG example.Example - exampleHandler received: 42
+```
 ### See Also
 
  * https://www.codingame.com/playgrounds/4497/configuring-logback-with-spring-boot
 
-
-### License
-This project is licensed under the terms of the MIT license.
-
 ### Author
 [Serguei Kouzmine](kouzmine_serguei@yahoo.com)
-
 
--- basic-logback/src/main/java/example/Example.java
+++ basic-logback/src/main/java/example/Example.java
@@ -1,20 +1,49 @@
 package example;
 
-import org.slf4j.Logger;
+import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// origin: https://github.com/eugenp/tutorials/tree/master/logging-modules/logback/src/main/java/com/baeldung/logback
-//
-public class Example {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+// origin: https://github.com/eugenp/tutorials/tree/master/logging-modules/logback/src/main/java/com/baeldung/logback
+// and https://github.com/maul5/example-springboot-logback2
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/example")
+public class Example {
 	private static final Logger logger = LoggerFactory.getLogger(Example.class);
 
-	public static void main(String[] args) {
-		logger.info("Example INFO message from {}", Example.class.getSimpleName());
-		logger.warn("Example WARN message from {}", Example.class.getSimpleName());
-		logger.debug("Example DEBUG message from {}",
-				Example.class.getSimpleName());
+	@PostConstruct // init-method
+	public void init() {
+		logger.info("init message");
+		logger.warn("init message");
+		// DEBUG will not be shown as long as root level is WARN
+		// NOTE: the Example.class.getSimpleName() will be added by transformer.
+		// No need to add explicitly
+		logger.debug("DEBUG {} init message", Example.class.getSimpleName());
 	}
 
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+		// can chain close
+		// SpringApplication.run(Example.class, args).close();
+	}
+
+	// https://www.baeldung.com/spring-request-param
+	@GetMapping
+	public String exampleHandler(@RequestParam(required = false) String data) {
+		// these logs are configured to be available only in the file
+		logger.info("exampleHandler received: {}", data);
+		logger.warn("exampleHandler received: {}", data);
+		logger.debug("exampleHandler received: {}", data);
+		return ("exampleHandler received: " + data);
+	}
 }
