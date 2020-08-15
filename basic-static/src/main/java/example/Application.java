407519559e4e2ee9de2efe0a448d4a06bbf4d748
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-08-15 00:39:22 -0400

    intermediate revision - replaced static index page with tmeleaf template with static resources linked

--- basic-static/src/main/java/example/Application.java
+++ basic-static/src/main/java/example/Application.java
@@ -2,11 +2,14 @@ package example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// https://qna.habr.com/q/734991
+import org.springframework.scheduling.annotation.EnableAsync;
+
 @SpringBootApplication
-public class Launcher {
+@EnableAsync
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Launcher.class, args);
+		SpringApplication.run(Application.class);
 	}
+
 }
---
+++ basic-static/src/test/java/example/selenium/support/CaseFormatTest.java
@@ -0,0 +1,15 @@
+package example.selenium.support;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CaseFormatTest {
+
+    @Test
+    public void convertsCamelCaseToLowerUnderscore() {
+        assertThat(CaseFormat.toLowerUnderscore("HomeControllerTest"))
+                .isEqualTo("home_controller_test");
+    }
+
+}
