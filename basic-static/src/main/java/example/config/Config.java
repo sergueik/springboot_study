284cc658f8ec9f2e78a15a510ebea74163abd986
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-09-19 01:42:23 +0200

    intermediate revision - added managed WebMvcConfigurer class

---
+++ basic-static/src/main/java/example/config/Config.java
@@ -0,0 +1,149 @@
+package example.config;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class Config implements WebMvcConfigurer {
+
+	private static final String projectPath = System.getProperty("user.dir")
+			.replaceAll("\\\\", "/");
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**")
+				.addResourceLocations("classpath:/static/images/", String.format(
+						"file:///%s/src/main/resources/static/images/", projectPath));
+		registry.addResourceHandler("/css/**")
+				.addResourceLocations("classpath:/static/css/");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> arg0) {
+		// TODO
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void addReturnValueHandlers(
+			List<HandlerMethodReturnValueHandler> arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureDefaultServletHandling(
+			DefaultServletHandlerConfigurer arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(
+			List<HandlerExceptionResolver> arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configurePathMatch(PathMatchConfigurer arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(
+			List<HandlerExceptionResolver> arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> arg0) {
+		// TODO
+
+	}
+
+	@Override
+	public MessageCodesResolver getMessageCodesResolver() {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public Validator getValidator() {
+		// TODO
+		return null;
+	}
+}
