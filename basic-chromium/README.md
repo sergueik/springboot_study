467bc5099e999c4cbd6e9709f94fc013f4d48cd9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-11-13 03:10:13 +0100

    intermediate revision - added PDF file download example (partiually working)

--- basic-chromium/README.md
+++ basic-chromium/README.md
@@ -3,7 +3,12 @@
 This directory contains a `Dockerfile` from [zenika/alpine-chrome](https://github.com/Zenika/alpine-chrome/blob/master/Dockerfile) but switched to __maven/jdk8__ apline base [image]( https://hub.docker.com/r/zenika/alpine-maven/tags) with maven and chromium to run the test suites. Note: the `chromium` binary installs some X libraries but is runnable in headless mode in console mode.
 
 ### Usage
-
+* compile test project. Note the need to remove the target directory
+```sh
+sudo rm -fr demo.project/target
+mvn test-compile
+mvn test
+```
 * build the `chromium` and `chromium-driver` into the image
 ```sh
 IMAGE='basic-maven-chromium'
@@ -34,6 +39,51 @@ docker container prune -f
 ### Note
 
 * in the current layout the `target` directory in `demo` project becomes owned by root account.
+
+### File Download Test
+
+```java
+@Test
+public void downloadPDF() {
+  url = "http://www.africau.edu/images/default/sample.pdf";
+  driver.get(url);
+  try {
+    Thread.sleep(5000);
+  } catch (InterruptedException e) {
+  }
+  File file = new File((downloadDirectory != null ? downloadDirectory : "/tmp") + "/" + "sample.pdf");
+  assertThat(file.exists(), is(false));
+  File f = new File(System.getProperty("user.dir") + "/" + "sample.pdf");
+  assertThat(f.exists(), is(true));
+}
+```
+```sh
+docker run -e DOWNLOAD_DIRECTORY=/tmp -it -v "$PWD/demo.selenium":/demo -w /demo $IMAGE mvn clean test ;  CONTAINER=$(docker container ls -a |grep 'basic-maven-chromium' | head -1 | cut -f1 -d ' '); docker container start $CONTAINER;docker exec -it $CONTAINER sh -c "find / -iname '*pdf' 2>/dev/null"
+```
+```sh
+fe728cfc1b7e
+/demo/sample.pdf
+```
+The setting seems to have no effect:
+```java
+
+chromeOptions.setExperimentalOption("prefs", new HashMap<String, Object>() {
+  {
+    put("profile.default_content_settings.popups", 0);
+    put("download.default_directory", downloadDirectory != null ? downloadDirectory : "/tmp");
+    put("download.prompt_for_download", false);
+    put("download.directory_upgrade", true);
+    put("safebrowsing.enabled", false);
+    put("plugins.always_open_pdf_externally", true);
+    put("plugins.plugins_disabled", new ArrayList() {
+      {
+        add("Chrome PDF Viewer");
+      }
+    });
+  }
+});
+```
+
 
 ### See Also
  * blog on [running Chrome in Docker](https://medium.com/@sahajamit/can-selenium-chrome-dev-tools-recipe-works-inside-a-docker-container-afff92e9cce5)
--- basic-chromium/demo.selenium/src/test/java/example/ChromiumBrowserTest.java
+++ basic-chromium/demo.selenium/src/test/java/example/ChromiumBrowserTest.java
@@ -11,10 +11,37 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ChromiumBrowserTest {
 
 	private WebDriver driver;
+	private static final List<String> plugins_disabled = new ArrayList<>();
+	static {
+		plugins_disabled.add("Chrome PDF Viewer");
+	};
+	private static final String downloadDirectory = System.getenv("DOWNLOAD_DIRECTORY");
+
+	private static Map<String, Object> prefs = new HashMap<>();
+	static {
+		prefs.put("profile.default_content_settings.popups", 0);
+		prefs.put("download.default_directory", downloadDirectory != null ? downloadDirectory : "/tmp");
+		prefs.put("download.prompt_for_download", false);
+		prefs.put("download.directory_upgrade", true);
+		prefs.put("safebrowsing.enabled", false);
+		prefs.put("plugins.always_open_pdf_externally", true);
+		prefs.put("plugins.plugins_disabled", plugins_disabled);
+	};
+
+	private static String url = "https://www.wikipedia.org/";
+	private static String cssSelector = "div.search-input > input[name=\"search\"]";
+	private final static String text = "Ice";
 
 	@Before
 	public void setUp() {
@@ -24,8 +51,33 @@ public class ChromiumBrowserTest {
 		// unknown error: Chrome failed to start: exited abnormally.
 		// (unknown error: DevToolsActivePort file doesn't exist)
 
-		options.addArguments( "--headless", "--window-size=1200x800", "--no-sandbox", "--remote-debugging-address=0.0.0.0", "--remote-debugging-port=9222", "--disable-gpu" );
+		options.addArguments("--headless", "--window-size=1200x800", "--no-sandbox",
+				"--remote-debugging-address=0.0.0.0", "--remote-debugging-port=9222", "--disable-gpu");
 		options.setBinary("/usr/bin/chromium-browser");
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("prefs", prefs);
+		// @formatter:off
+
+		// alternatively, inline - i find it less readable
+
+		chromeOptions.setExperimentalOption("prefs", new HashMap<String, Object>() {
+			{
+				put("profile.default_content_settings.popups", 0);
+				put("download.default_directory", downloadDirectory != null ? downloadDirectory : "/tmp");
+				put("download.prompt_for_download", false);
+				put("download.directory_upgrade", true);
+				put("safebrowsing.enabled", false);
+				put("plugins.always_open_pdf_externally", true);
+				put("plugins.plugins_disabled", new ArrayList() {
+					{
+						add("Chrome PDF Viewer");
+					}
+				});
+			}
+		});
+
+		// @formatter:on
 		driver = new ChromeDriver(options);
 	}
 
@@ -34,21 +86,28 @@ public class ChromiumBrowserTest {
 		driver.close();
 	}
 
-	private final static String url = "https://www.wikipedia.org/";
-	private final static String cssSelector = "div.search-input > input[name=\"search\"]";
-	private final static String text = "Ice";
+	@Test
+	public void downloadPDF() {
+		url = "http://www.africau.edu/images/default/sample.pdf";
+		driver.get(url);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		}
+		File file = new File((downloadDirectory != null ? downloadDirectory : "/tmp") + "/" + "sample.pdf");
+		assertThat(file.exists(), is(false));
+		file = new File(System.getProperty("user.dir") + "/" + "sample.pdf");
+		assertThat(file.exists(), is(true));
+	}
 
 	@Test
 	public void canSearch() {
+		url = "https://www.wikipedia.org/";
 		driver.get(url);
 		driver.findElement(By.cssSelector(cssSelector)).sendKeys(text);
 		driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
 
 		assertThat(driver.getTitle(), containsString(text));
 	}
-	// TODO:
-	// [INFO] Skipping execution of surefire because it has already been run for
-	// this configuration
 
 }
-
