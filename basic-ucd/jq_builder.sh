518c68c3cb44f9f06be5c789630451cb50e43e70
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-12-11 01:39:42 -0500

    intermediate revision - added relted resource reporting

--- basic-ucd/jq_builder.sh
+++ basic-ucd/jq_builder.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+if [[ -z "${DEBUG}" ]] ; then
+  echo 'DEBUG was not set explicitly, default is false'
+  DEBUG='false'
+fi
+echo "DEBUG=${DEBUG}"
+
 HOSTNAME=${1:-host1}
 QUERY=".[]|select(.type=\"agent\")|select(.name|match(\"$HOSTNAME.\"))|.name"
-#  for chained calls, '.path' at the end will be more useful
+# for chained calls, '.path' at the end will be more useful
 UCD_URL=https://localhost:8443
 RESOURCE_ID='/TEST'
 TEMP_FILE1=/tmp/a.txt
@@ -13,28 +20,28 @@ else
   cat <<EOF>$TEMP_FILE1
 [ 
   {
-  "id": "1122334455-ffff-aaaa-bbbb-10101010",
-  "path": "/TEST/host1.domain_use_agent_name",
-  "active": true,
-  "prototype": false,
-  "name": "host1.domain_use_agent_name",
-  "description": "ucd agent on host1",
-  "hasAgent": true,
-  "status": "ONLINE",
-  "type": "agent",
-  "tags": []
+    "id": "1122334455-ffff-aaaa-bbbb-10101010",
+    "path": "/TEST/host1.domain_use_agent_name",
+    "active": true,
+    "prototype": false,
+    "name": "host1.domain_use_agent_name",
+    "description": "ucd agent on host1",
+    "hasAgent": true,
+    "status": "ONLINE",
+    "type": "agent",
+    "tags": []
   },
   {
-  "id": "1122334455-ffff-aaaa-bbbb-10101010",
-  "path": "/TEST/host2.domain_use_agent_name",
-  "active": true,
-  "prototype": false,
-  "name": "host2.domain_use_agent_name",
-  "description": "ucd agent on host2",
-  "hasAgent": true,
-  "status": "ONLINE",
-  "type": "agent",
-  "tags": []
+    "id": "1122334455-ffff-aaaa-bbbb-10101010",
+    "path": "/TEST/host2.domain_use_agent_name",
+    "active": true,
+    "prototype": false,
+    "name": "host2.domain_use_agent_name",
+    "description": "ucd agent on host2",
+    "hasAgent": true,
+    "status": "ONLINE",
+    "type": "agent",
+    "tags": []
   }
 ]
 EOF
