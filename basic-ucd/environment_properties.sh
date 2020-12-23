2264ca93af91cd45b678235a8f51da91e9cefac4
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2020-12-23 06:21:51 +0100

    added breadcrumps of retrieving component role properties

--- basic-ucd/environment_properties.sh
+++ basic-ucd/environment_properties.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Steps:
+# 1. /cli/resource/parent?=$ENVIRONMENT_RESOURCE_ID => $AGENT_NAME
+# 2. /cli/resource/parent?=$AGENT_NAME_URLECODED => $COMPONENT_ID based on $COMPONENT_NAME
+# 3. /rest/resource/resource/$COMPONENT_ID/roles => propDefs
+# 4. /rest/resource/resource/$COMPONENT_ID => JSON with several useful nodes:
+# QUERY='.role,.roleProperties'
+# OPTIONS=
+# so extract ROLE_ID
+# QUERY='.role|.id'
+# to extract ROLE_PROPERTIES from component resource
+# QUERY='.roleProperties|to_entries[]' 
+# NOTE: there is a special API for the same. It needs component id and role id
+# getResourceRoleProperties
 # see also: 
 # https://github.com/UrbanCode/uDeployRestClient/blob/master/src/main/java/com/urbancode/ud/client/ResourceClient.java#L379
 #
@@ -18,5 +31,16 @@ ROLE_ID=''
 ) > $TMP_FILE
 sleep 1
 cat $TMP_FILE |jq '.'
-
-# getResourceRoleProperties
+OPTIONS='-cr'
+QUERY=".[]|select(.name|test(\"${COMPONENT_NAME}\"))|.id"
+COMPONENT_ID=$(
+jq $OPTIONS $QUERY < $TMP_FILE | while read DATA ; do
+  echo $DATA
+done
+)
+QUERY='.roleProperties|to_entries[]'
+PARAMS=$(
+jq $OPTIONS $QUERY < $TMP_FILE | jq --splurp '.[]|.key,.value' |while read DATA ; do
+  echo $DATA
+done
+)
