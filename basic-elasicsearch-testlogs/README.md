1709fb3c517fb0486f7cd8e27ecb2495dd3fc420
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-03-25 03:57:58 +0100

    intermediate revision - ugraded to ElasticSearh 7.x Virtual Box, done anddocumented the security testing

--- basic-elasicsearch-testlogs/README.md
+++ basic-elasicsearch-testlogs/README.md
@@ -1,6 +1,8 @@
 #### Info
 
-This directory contains a replica of [Demo client test logging project](https://github.com/dadoonet/elasticsearch-integration-tests) tweaked from 7.7.1 to 6.6.2 to be able injest data to ELK 6.6.2 instandlaone Virtual Box [apolloclark/ubuntu16.04-elk](https://app.vagrantup.com/apolloclark/boxes/ubuntu16.04-elk/versions/20190426). Not 100% functionality works at Java level, but the indexed data is visible in Kibana aftert the run.
+This directory contains a replica of [Demo client test logging project](https://github.com/dadoonet/elasticsearch-integration-tests) 
+
+indexing the test logs on ElasticSearch 7.7.1 [Virtual Box](https://app.vagrantup.com/demonium/boxes/u18-elk/versions/20191110/providers/virtualbox.box)
 
 ### Usage
 
@@ -14,6 +16,12 @@ curl -X PUT http://localhost:9200/$INDEX
 mvn -Dtests.cluster.host=192.168.0.25 clean test
 ```
 in the above command specify the ip address of the host the Virtual Box with ELK is running (assuming it is configured to user NAT). When not set, defaults to `hostname -i`
+Alternatively set in
+```sh
+src/test/resources/application.properties
+```
+(note this app does not scan `src/main/resources` location by design
+see the result in Kibana
 
 this will log to console:
 ```sh
@@ -26,9 +34,99 @@ if you also see the exception,
 it is a result of back porting of API across incompatible revisions of [ElasticSearch RESTFul Client](https://github.com/elastic/elasticsearch) which was not the focus of this exercise and can be ignored.
 
 
-* see the result in Kibana
+* see the result in Kibana (NOTE: the screen shot taken when working with __ELK 6.6.2__)
 
 ![Example](https://github.com/sergueik/springboot_study/blob/master/basic-elasicsearch-testlogs/screenshots/capture.png)
+
+* change password
+```sh
+pushd /usr/share/elasticsearch/bin
+./elasticsearch-setup-passwords interactive
+```
+* on vanilla box, you will get the error
+```sh
+Unexpected response code [500] from calling GET http://10.0.2.15:9200/_security/_authenticate?pretty
+It doesn't look like the X-Pack security feature is enabled on this Elasticsearch node.
+Please check if you have enabled X-Pack security in your elasticsearch.yml configuration file.
+
+ERROR: X-Pack Security is disabled by configuration.
+```
+* proceed with adding 
+```sh
+xpack.security.enabled: true
+discovery.type: single-node
+```
+to `/etc/elasticsearch/elasticsearch.yml`. Then repeat command
+```sh
+pushd /usr/share/elasticsearch/bin
+./elasticsearch-setup-passwords interactive
+```
+```sh
+Enter password for [elastic]:
+Reenter password for [elastic]:
+Enter password for [apm_system]:
+Reenter password for [apm_system]:
+Enter password for [kibana]:
+Reenter password for [kibana]:
+Enter password for [logstash_system]:
+Reenter password for [logstash_system]:
+Enter password for [beats_system]:
+Reenter password for [beats_system]:
+Enter password for [remote_monitoring_user]:
+Reenter password for [remote_monitoring_user]:
+Changed password for user [apm_system]
+Changed password for user [kibana]
+Changed password for user [logstash_system]
+Changed password for user [beats_system]
+Changed password for user [remote_monitoring_user]
+Changed password for user [elastic]
+```
+
+NOTE: can only do it once. Repeated runs will show the following
+```sh
+./elasticsearch-setup-passwords interactive
+```
+```sh
+Possible causes include:
+ * The password for the 'elastic' user has already been changed on this cluster
+ * Your elasticsearch node is running against a different keystore
+   This tool used the keystore at /etc/elasticsearch/elasticsearch.keystore
+
+ERROR: Failed to verify bootstrap password
+```
+
+The attempt to run with invalid credentials (defined in `application.properties`will result in 
+```sh
+org.elasticsearch.client.ResponseException: method [GET], host [http://192.168.0.25:9200], URI [/], status line [HTTP/1.1 401 Unauthorized]
+
+{
+  "error": {
+    "root_cause": [
+      {
+        "type": "security_exception",
+        "reason": "failed to authenticate user [elastic]",
+        "header": {
+          "WWW-Authenticate": "Basic realm=\"security\" charset=\"UTF-8\""
+        }
+      }
+    ],
+    "type": "security_exception",
+    "reason": "failed to authenticate user [elastic]",
+    "header": {
+      "WWW-Authenticate": "Basic realm=\"security\" charset=\"UTF-8\""
+    }
+  },
+  "status": 401
+}
+```
+after restoring the proper credentials, the error goes away
+NOTE: `try...catch` around the code throwing this exception will not catch it.
+### See Also
+
+* [documentation](https://www.elastic.co/guide/en/elasticsearch/client/java-rest/master/java-rest-high.html)
+* [REST Client](https://github.com/gentics/elasticsearch-java-client) not using any ElasticSeruch assemblies
+* springboot __1.5.6__ elasticsearch __5.6.3__ [client](https://github.com/altfatterz/elasticsearch-java-client-demo/blob/master/pom.xml)
+* https://www.elastic.co/guide/en/elasticsearch/reference/current/setup-passwords.html
 
 ### Author
 [Serguei Kouzmine](kouzmine_serguei@yahoo.com)
--- basic-elasicsearch-testlogs/src/test/java/example/integration/ElasticsearchTest.java
+++ basic-elasicsearch-testlogs/src/test/java/example/integration/ElasticsearchTest.java
@@ -7,6 +7,7 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.ActionRequestValidationException;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
@@ -22,54 +23,68 @@ import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.bytes.BytesReference;
 // not available in 6.2.0
-// import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
-
+import java.util.Properties;
+import java.io.FileInputStream;
 import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class ElasticsearchTest {
 
-	private static final Logger logger = LogManager
-			.getLogger(ElasticsearchTest.class);
+	private static final Logger logger = LogManager.getLogger(ElasticsearchTest.class);
 	private static RestHighLevelClient client;
 	private static final String INDEX = "scenario1";
 
 	@BeforeClass
 	public static void startElasticsearchRestClient() throws IOException {
 		// does not currently have properties ser
-		int testClusterPort = Integer
-				.parseInt(System.getProperty("tests.cluster.port", "9200"));
-		// use NAT
-		String testClusterHost = System.getProperty("tests.cluster.host",
+
+		Properties properties = new Properties();
+		InputStream input = null;
+		String propertiesFile = "application.properties";
+		// TODO: prepend server root
+		input = new FileInputStream(
+				String.format("%s/src/test/resources/%s", System.getProperty("user.dir"), propertiesFile));
+		properties.load(input);
+		int testClusterPort = Integer.parseInt(properties.getProperty("tests.cluster.port", "9200"));
+		String testClusterHost = properties.getProperty("tests.cluster.host",
 				InetAddress.getLocalHost().getHostAddress());
-		String testClusterScheme = System.getProperty("tests.cluster.scheme",
-				"http");
-		String testClusterUser = System.getProperty("tests.cluster.user",
-				"elastic");
-		String testClusterPass = System.getProperty("tests.cluster.pass",
-				"changeme");
+		String testClusterScheme = properties.getProperty("tests.cluster.scheme", "http");
+		String testClusterUser = properties.getProperty("tests.cluster.user", "elastic");
+		String testClusterPass = properties.getProperty("tests.cluster.pass", "changemenow");
 
-		logger.info("Starting a client on {}://{}:{}", testClusterScheme,
-				testClusterHost, testClusterPort);
+		logger.info("Starting a client on {}://{}:{}", testClusterScheme, testClusterHost, testClusterPort);
 
-		// We start a client
-		RestClientBuilder builder = getClientBuilder(
-				new HttpHost(testClusterHost, testClusterPort, testClusterScheme),
+		// start a client
+		RestClientBuilder builder = getClientBuilder(new HttpHost(testClusterHost, testClusterPort, testClusterScheme),
 				testClusterUser, testClusterPass);
+
+		// Without xpack enabled the credentials are ignored
+		logger.info("Starting a request builder with credentials {}:{}", testClusterUser, testClusterPass);
+
 		client = new RestHighLevelClient(builder);
 
-		// We make sure the cluster is running
-		MainResponse info = client.info(RequestOptions.DEFAULT);
-		logger.info("Client is running against an elasticsearch cluster {}.",
-				info.getVersion().toString());
+		// make sure the cluster is running
+		try {
+			MainResponse info = client.info(RequestOptions.DEFAULT);
+			logger.info("Client is running against an elasticsearch cluster {}.", info.getVersion().toString());
+		} catch (org.elasticsearch.client.ResponseException e) {
+			logger.error("Response Exception: {}", e);
+			// not reached
+			throw new RuntimeException(e);
+
+		}
 	}
 
 	@AfterClass
@@ -80,78 +95,62 @@ public class ElasticsearchTest {
 		}
 	}
 
-	private static RestClientBuilder getClientBuilder(HttpHost host,
-			String username, String password) {
+	// see
+	// https://github.com/elastic/elasticsearch/blob/master/client/rest/src/main/java/org/elasticsearch/client/RestClientBuilder.java
+	private static RestClientBuilder getClientBuilder(HttpHost host, String username, String password) {
 		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-		credentialsProvider.setCredentials(AuthScope.ANY,
-				new UsernamePasswordCredentials(username, password));
+		credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-		return RestClient.builder(host)
-				.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder
-						.setDefaultCredentialsProvider(credentialsProvider));
+		return RestClient.builder(host).setHttpClientConfigCallback(
+				httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
 	}
 
 	@Test
 	public void test1() throws IOException {
-		// We remove any existing index
+		// remove existing index, to be sure in the number of entries found later
 		try {
 			logger.info("-> Removing index {}.", INDEX);
-			client.indices().delete(new DeleteIndexRequest(INDEX),
-					RequestOptions.DEFAULT);
+			client.indices().delete(new DeleteIndexRequest(INDEX), RequestOptions.DEFAULT);
 		} catch (ElasticsearchStatusException e) {
 			assertThat(e.status().getStatus(), is(404));
 		}
 
-		// We create a new index
-		// need 7.x
-		//
-		// logger.info("-> Creating index {}.", INDEX);
-		// client.indices().create(new CreateIndexRequest(INDEX),
-		// RequestOptions.DEFAULT);
+		// create a new index
+		// NOTE: needs 7.x
+		logger.info("-> Creating index {}.", INDEX);
+		client.indices().create(new CreateIndexRequest(INDEX), RequestOptions.DEFAULT);
 
-		// We index some documents
+		// index some documents
 		logger.info("-> Indexing one document in {}.", INDEX);
-
-		XContentBuilder data = jsonBuilder().startObject().field("foo", "bar")
-				.endObject();
-		logger.info("Sending " + data.contentType() + " "
-				+ BytesReference.bytes(data).utf8ToString());
+		// NOTE: the data information appears to be ignored, need use some other API
+		XContentBuilder data = jsonBuilder().startObject().field("key", "data").endObject();
+		logger.info("Sending " + data.contentType() + " " + BytesReference.bytes(data).utf8ToString());
 
 		// https://github.com/elastic/elasticsearch/blob/master/server/src/main/java/org/elasticsearch/action/index/IndexRequest.java#L375
 		// https://github.com/elastic/elasticsearch/blob/v6.6.2/server/src/main/java/org/elasticsearch/action/index/IndexRequest.java#L389
 		// java.lang.IllegalArgumentException: The number of object passed must be
 		// even but was [1]
+		// filled the missing argument to have call go through
 		IndexRequest indexRequest = new IndexRequest(INDEX).source(data,
-				new Object[] { "foo", "bar"});
+				new Object[] { "information about the test", "more information", null, null });
 
 		try {
-			IndexResponse ir = client.index(
-					indexRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
+			IndexResponse ir = client.index(indexRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE),
 					RequestOptions.DEFAULT);
 			logger.info("-> Document indexed with _id {}.", ir.getId());
 
 		} catch (ActionRequestValidationException e) {
-			// org.elasticsearch.action.ActionRequestValidationException: Validation
-			// Failed: 1: type is missing;
 			logger.info("Exception(ignored) {}", e.toString());
 		}
 
-		// We search
+		// search
 		try {
-			SearchResponse sr = client.search(new SearchRequest(INDEX),
-					RequestOptions.DEFAULT);
-			logger.info("{}", sr);
-			assertThat(sr.getHits().getTotalHits(), is(1L));
-			// assertThat(sr.getHits().getTotalHits().value, is(1L));
+			SearchResponse searchResponse = client.search(new SearchRequest(INDEX), RequestOptions.DEFAULT);
+			logger.info("Response: {}", searchResponse);
+			assertThat(searchResponse.getHits().getTotalHits().value, is(1L));
+			assertThat(searchResponse.getHits().getAt(0).getIndex(), is("scenario1"));
 		} catch (ElasticsearchStatusException e) {
 			logger.info("Exception(ignored) {}", e.toString());
-			// ElasticsearchStatusException[Elasticsearch exception
-			// [type=illegal_argument_exception, reason=request [/scenario1/_search]
-			// contains unrecognized parameter: [ccs_minimize_roundtrips]]]
-			// https://stackoverflow.com/questions/55602377/elasticsearchstatusexception-contains-unrecognized-parameter-ccs-minimize-roun
-
-			// ElaseticsearchStatusException[Elasticsearch exception
-			// [type=index_not_found_exception,reason=no such index]]
 		}
 	}
 }
