c2c774ace3b63975d918f601014b2f384a59de5d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2021-08-04 18:45:34 -0400

    added note of failingintegration tests class that could be usefil for jsp testing had it not failed

--- basic/src/test/java/example/controller/MVCRealTest.java
+++ basic/src/test/java/example/controller/MVCRealTest.java
@@ -26,12 +26,17 @@ import example.controller.Controller;
 import example.service.ExampleService;
 import example.ExampleApplication;
 
+// TODO: at commit 0d9bd8110fb26138420a0297a48049c1f49604a0
+// all tests in MVCRealTest are broken
+// with No mapping found for HTTP request with URI [/hello]
+// and subsequent assertion errors
+
 // NOTE: @Runwith annotation with real classes crashes the JVM
 // @RunWith(SpringJUnit4ClassRunner.class)
 @WebMvcTest
 public class MVCRealTest {
 
-	final static String route = "/basic";
+	final static String route = "/hello";
 	final static String body = "Hello basic";
 	private static String charset = null;
 	private ResultActions resultActions;
@@ -55,29 +60,36 @@ public class MVCRealTest {
 
 	// examine HTTP status
 	@Test
+	// TODO: MVCRealTest.statusTest:59 Status expected:<200> but was:<404>
 	public void statusTest() throws Exception {
 		resultActions.andExpect(status().isOk());
 	}
 
 	// examine body
 	@Test
+	// TODO: Response content Expected: a string containing "Hello basic" but: was
+	// ""
 	public void bodyTest() throws Exception {
 		resultActions.andExpect(content().string(containsString(body)));
 	}
 
 	// examine value
+	// TODO: No value at JSON path "$.name", exception: json can not be null or
+	// empty
 	@Test
 	public void jsonTest() throws Exception {
 		mvc.perform(get(route + "/json")).andExpect(jsonPath("$.name", is(body)));
 	}
 
 	// count nodes
+	// TODO: No value at JSON path "$.*", exception: json can not be null or empty
 	@Test
 	public void jsonTest2() throws Exception {
 		mvc.perform(get(route + "/json")).andExpect(jsonPath("$.*", hasSize(1)));
 	}
 
 	// examine response header
+	// TODO: Content type not set
 	@Test
 	// NOTE: these expectations are Junit version sensitive
 	public void contentTypeTest() throws Exception {
