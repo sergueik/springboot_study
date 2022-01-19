898f8e1c243951724674accc51a7c04b786e8edb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-01-19 02:04:40 +0100

    intermediate revision - removed redundant source directory

--- basic-mongo/src/main/java/example/ModelMongoRepository.java
+++
@@ -1,6 +0,0 @@
-package example;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ModelMongoRepository extends PagingAndSortingRepository<Model, String> {
-}     
