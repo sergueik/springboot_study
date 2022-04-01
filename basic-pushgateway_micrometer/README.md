94b5d8ddf1152d60e7da534b0c9f55cd37c1c1e3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-01 19:04:02 -0400

    intermediate revision - added alternative adapter - "micrometer" - example with mockito unit tests

---
+++ basic-pushgateway_micrometer/README.md
@@ -0,0 +1,15 @@
+### Info
+
+
+replica of [Spring Boot Prometheus PushGateway Demo](https://github.com/ramesh-lingappan/prometheus-pushgateway-demo) a demo [Spring Boot](https://spring.io/blog/2020/08/13/spring-boot-2-3-3-available-now) console app using
+[Micrometer](https://github.com/micrometer-metrics/micrometer) jar adapter to publish metrics to
+[Prometheus](https://prometheus.io) via [Prometheus Push Gateway](https://github.com/prometheus/pushgateway) that would preserve 
+and later expose these metrics to [Prometheus](https://prometheus.io).
+Added unit tests through [Mockito](https://site.mockito.org)
+
+
+### See Also
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
+
---
+++ basic-pushgateway_micrometer/src/main/java/example/ExampleService.java
@@ -0,0 +1,29 @@
+package example;
+
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+
+	private final Counter counter;
+
+	public ExampleService(MeterRegistry meterRegistry) {
+		this.counter = meterRegistry.counter("hello.counter", "type", "hello");
+	}
+
+	@Timed(value = "hello-process", description = "Time spent processing hello", extraTags = {
+			"type", "hello" })
+	public String getHello() {
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		counter.increment();
+		return "Hello, counter = " + counter.count();
+	}
+}
