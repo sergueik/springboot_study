8c2cfe04aaa6050c5f1c94183ba337d26cf31ede
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-02 02:33:36 +0200

    intermediate revision -updated the shell script to not require bash and adjust columns

--- basic-pushgateway-bash/README.md
+++ basic-pushgateway-bash/README.md
@@ -7,7 +7,7 @@ Few bash and shell scripts to publish metrics to pushgateway
 ```sh
 NAME=pushgateway
 IMAGE=pushgateway
-docker build -f Dockerfile.$IMAGE -t $IMAGE . 
+docker build -f Dockerfile.$IMAGE -t $IMAGE .
 docker container rm -f pushgateway
 docker run --name $NAME -p 9091:9091 -d $IMAGE
 ```
@@ -28,6 +28,49 @@ cpu_usage{instance="lenovoy40-1",job="cpu_usage",pid="6",process="[mm_percpu_wq]
 push_failure_time_seconds{instance="lenovoy40-1",job="cpu_usage"} 0
 push_time_seconds{instance="lenovoy40-1",job="cpu_usage"} 1.6488571168761053e+09
 ```
+### Test in Container
+
+NOTE: The default shell in [alpine](https://alpinelinux.org) is not bash an `ps` does not show `%CPU`
+
+* create a vanilla container
+```sh
+docker run --link pushgateway -v $(pwd):/tmp -w /tmp  -it alpine:3.9.5 sh
+```
+* in the container, add `curl`.
+```
+apk add curl
+```
+* publish metrics
+```
+./cpu_usage.sh
+```
+it will log to console
+```text
+cpu_usage{process="sh", pid="1"} 1592
+ cpu_usage{process="vi", pid="517"} 1532
+ cpu_usage{process="cpu_usage.sh", pid="1238"} 1588
+ cpu_usage{process="ps", pid="1321"} 1516
+
+```
+* leave and remove the container
+```
+docker container prune -f
+```
+* verify the metrics	
+
+```sh
+curl -s http://localhost:9091/metrics | grep -i cpu_u
+```
+```text
+# TYPE cpu_usage untyped
+cpu_usage{instance="9bc14c8d2fd5",job="cpu_usage",pid="1",process="sh"} 1592
+cpu_usage{instance="9bc14c8d2fd5",job="cpu_usage",pid="1238",process="cpu_usage.sh"} 1588
+cpu_usage{instance="9bc14c8d2fd5",job="cpu_usage",pid="2041",process="ps"} 1516
+cpu_usage{instance="9bc14c8d2fd5",job="cpu_usage",pid="517",process="vi"} 1532
+```
+
+
+
 ### See Also
  * [introduction to pushing metrics](https://prometheus.io/docs/instrumenting/pushing/
 ### Author
--- basic-pushgateway-bash/cpu_usage.sh
+++ basic-pushgateway-bash/cpu_usage.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 # based on: https://github.com/SCHKN/pushgateway-scripts/blob/master/cpu_usage
 # Parsing ps aux lines, formats them and sends them to pushgateway.
 INSTANCE=$(/bin/hostname -f)
 JOB=${0##*/} # remove the path
-## Pushgateway VARs
+## Pushgateway vars
 HOST=${1:-pushgateway}
 PORT=9091
 # remove the extension
@@ -13,21 +13,42 @@ INTANCE=${INSTANCE%.*}
 PATH="/metrics/job/${JOB}/instance/${INSTANCE}"
 URL="http://${HOST}:${PORT}${PATH}"
 # e.g. http://localhost:9091/metrics/job/top/instance/machine
-while /bin/sleep 1
+INTERVAL=1
+LINES=5
+TMPFILE="/tmp/data.$$"
+
+1>&2 echo "TMPFILE=${TMPFILE}"
+while /bin/sleep $INTERVAL
 do
-COMMAND_OUTPUT=$(/bin/ps aux| /usr/bin/head -5)
-VAR=''
-while read -r COMMAND_OUTPUT
-# below is bash syntax, not recognized by older shells:
-# syntax error: unexpected redirection
-# Syntax error: redirection unexpected
+# NOTE: space-sensitive ?
+# NO %CPU on alpine
+1>$TMPFILE 2>/dev/null /bin/ps aux -o pid,comm,vsz | /usr/bin/head -$LINES
+# NOTE the head has no effect with output redirection this way
+
+1>&2 /usr/bin/wc $TMPFILE
+VAR=$(
+/bin/cat $TMPFILE |/usr/bin/head -$LINES| while read LINE
 do
-   VAR=$VAR$(/usr/bin/awk '{print "cpu_usage{process=\""$11"\", pid=\""$2"\"}", $3z}');
-done <<< "$COMMAND_OUTPUT"
-echo $VAR
+if echo $LINE |/bin/grep -q 'VSZ' ; then
+  echo >/dev/null
+else
+ROW=$(echo $LINE |/usr/bin/awk '{print "cpu_usage{process=\""$2"\", pid=\""$1"\"}", $3z}')
+echo $ROW'NL'
+fi
+done
+)
+# hack around losing the VAR after exising the loop
+# may lead toa problem with miltiple entries on the same line
+
+# https://stackoverflow.com/questions/16414410/delete-empty-lines-using-sed
+# NOTE: 'chomp' does not work with /^\\s*$/d
+echo '---'
+echo $VAR|/bin/sed 's|NL|\n|g' | /bin/sed '/^[[:space:]]*$/d'
+echo '---'
 if /bin/true ; then
-/usr/bin/curl -X POST -H 'Content-Type: text/plain' --data "$VAR
+/usr/bin/curl -X POST -H 'Content-Type: text/plain' --data "$(echo $VAR|/bin/sed 's|NL|\n|g'| /bin/sed '/^[[:space:]]*$/d')
 " $URL
 fi
 done
+
 
