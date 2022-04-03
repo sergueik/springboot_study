22c9151c0af719034c381ac5d01f1d9fb7badd50
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-03 04:25:10 +0200

    intermediate revision - added the twin script, commented on some effects not learned earlier

--- basic-pushgateway-bash/memory_usage.sh
+++ basic-pushgateway-bash/memory_usage.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-# origin: https://github.com/SCHKN/pushgateway-scripts/blob/master/memory_usage
+# based on: https://github.com/SCHKN/pushgateway-scripts/blob/master/memory_usage
 # Parsing ps aux lines, formats them and sends them to pushgateway.
-while sleep 1 
+# identical to cpu_usage.sh - the only difference is in
+# the field processing awk script expression
+# USER PID %CPU %MEM VSZ RSS TTY STAT START TIME COMMAND
+# 1    2   3    4    5   6   7   8    9     10   11
+while sleep 1
 do
-z=$(ps aux)
-var=""
-while read -r z 
+# using default ps fields - this will work on RHEL or Debian but not Alpine
+COMMAND='ps aux'
+COMMAND_OUTPUT=$($COMMAND)
+VAR=''
+while read -r COMMAND_OUTPUT
 do
-   var=$var$(awk '{print "memory_usage{process=\""$11"\", pid=\""$2"\"}", $4z}');
-done <<< "$z"
-echo "$var"
-curl -X POST -H  "Content-Type: text/plain" --data "$var
+#if grep -q '%MEM' ; then
+#  continue
+#fi
+  VAR=$VAR$(/usr/bin/awk '{print "memory_usage{process=\""$11"\", pid=\""$2"\"}", $4z}');
+done <<< "$COMMAND_OUTPUT"
+# NOTE: preserving newlines by enclosing in the quotes
+echo "$VAR"
+if /bin/false ; then
+curl -X POST -H  "Content-Type: text/plain" --data "$VAR
 " http://localhost:9091/metrics/job/top/instance/machine
+fi
 done
+
