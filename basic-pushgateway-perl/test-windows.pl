b9654d1a781b7e1b4cc2193b325a0d701624a1e7
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-04-20 19:14:22 -0400

    intermediate revision - renamed the Dockerfile to match documentation, tidied the source, tested gauge

--- basic-pushgateway-perl/test-windows.pl
+++ basic-pushgateway-perl/test-windows.pl
@@ -1,22 +1,25 @@
 use Pushgateway::TinyActiveState;
 
-my $job_name = 'my_custom_metrics';
+my $job_name      = 'my_custom_metrics';
 my $instance_name = $ENV{'COMPUTERNAME'};
-my $team = 'test';
-my $opt =  {
- 'instance_name' => $instance_name,
-  '-host' => '192.168.0.64',
-  '-port' => 9091,
-  '-path' => "/metrics/job/${job_name}/instance/${instance_name}/team/${team}",
-  '-timeout' => 10 };
-my $o = Pushgateway::TinyActiveState->new(%$opt );
+my $team          = 'test';
+my $opt           = {
+    'instance_name' => $instance_name,
+    '-host'         => '192.168.0.64',
+    '-port'         => 9091,
+    '-path' =>
+      "/metrics/job/${job_name}/instance/${instance_name}/team/${team}",
+    '-timeout' => 10
+};
+my $o = Pushgateway::TinyActiveState->new(%$opt);
+
 # NOTE: setter does not work
 # $o->{'instance_name'} = $instance_name;
 
 my $custom = {
-  '-metric_name' => 'test',
-  '-label' => {},
-  '-value' => 42
- };
-$o -> add(%$custom);
+    '-metric_name' => 'test',
+    '-label'       => {},
+    '-value'       => 42
+};
+$o->add(%$custom);
 
--- basic-pushgateway-perl/test.pl
+++ basic-pushgateway-perl/test.pl
@@ -5,42 +5,61 @@ use Socket;
 use Data::Dumper;
 use strict;
 
-
-my $value      = 1;
+my $value = 1;
+my $file  = undef;
 
 GetOptions(
- 'value=s'  => \$value
+    'value=s' => \$value,
+    '-file=s' => \$file
 );
 
 # NOTE non-standard param key names inherited from Net::Prometheus::Pushgateway
 
-my $job_name = 'my_custom_metrics';
-my $addr = inet_ntoa((gethostbyname(hostname))[4]);
+my $job_name      = 'my_custom_metrics';
+my $addr          = inet_ntoa( ( gethostbyname(hostname) )[4] );
 my $instance_name = hostname . '(' . $addr . ')';
+
 # https://stackoverflow.com/questions/4510550/using-perl-how-do-i-decode-or-create-those-encodings-on-the-web
 print STDERR $addr;
 $instance_name =~ s/([^^A-Za-z0-9\-_.!~*'()])/ sprintf "%%%0x", ord $1 /eg;
 print STDERR $instance_name;
 my $team = 'test';
-my $opt =  {
-  '-host' => 'pushgateway',
-  '-port' => 9091,
-  '-path' => "/metrics/job/${job_name}/instance/${instance_name}/team/${team}",
-  '-timeout' => 10 };
-my $o = Pushgateway::Tiny->new(%$opt );
+my $opt  = {
+    '-host' => 'pushgateway',
+    '-port' => 9091,
+    '-path' =>
+      "/metrics/job/${job_name}/instance/${instance_name}/team/${team}",
+    '-timeout' => 10
+};
+my $o      = Pushgateway::Tiny->new(%$opt);
 my $custom = {
-  '-metric_name' => 'test',
-  '-label' => {},
-  '-value' => 42
- };
- # NOTE: the leading slash is required
- # '/test'  404
- # '/'      405
- # 'test'   599
+    '-metric_name' => 'test',
+    '-label'       => {},
+    '-value'       => 42
+};
 
-$o -> add(%$custom);
+# NOTE: the leading slash is required
+# '/test'  404
+# '/'      405
+# 'test'   599
+
+$o->add(%$custom);
 print STDERR "\n" . 'Payload:' . "\n" . $o->{raw_str};
 
-$o->increment(-metric_name => 'perl_counter', -label => {'perl_label' => 'custom label'}, -value => $value); 
+$o->increment(
+    -metric_name => 'perl_counter',
+    -label       => { 'perl_label' => 'custom label' },
+    -value       => $value
+);
 print STDERR "\n" . 'Payload:' . "\n" . $o->{raw_str};
+my $gauge = {
+    '-metric_name' => 'test_gauge',
+    '-value'       => 10,
+    '-label'       => {
+        'instance' => 'node1'
+    }
+};
+$o->gauge(%$gauge);
+print STDERR "\n" . 'Payload:' . "\n" . $o->{raw_str};
+
 __END__
