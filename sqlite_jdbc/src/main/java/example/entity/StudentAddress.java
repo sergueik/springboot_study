82fee540a41f43969df438ae25b61695092a3a90
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-05-06 02:01:50 +0200

    intermediate revision - replaced composition class StudentAddress with a flat copy of the two joining pojo classes field by field - not good for maintenance but will work to collect data

--- sqlite_jdbc/src/main/java/example/entity/StudentAddress.java
+++ sqlite_jdbc/src/main/java/example/entity/StudentAddress.java
@@ -1,76 +1,100 @@
 package example.entity;
 
 public class StudentAddress {
-	private Student student;
-	private Address address;
+
+	private long addressId;
+	private String street;
+	private String city;
+	private String state;
+	private String zipcode;
+	private long id;
+	private String name;
+	private String course;
+
+	// NOTE: these fields are set via reflection
+	// dispatch the setters and getters wil lnot work
 
 	public long getId() {
-		return student.getId();
+		// return student.getId();
+		return id;
 	}
 
 	public void setId(long value) {
-		student.setId(value);
+		// student.setId(value);
+		id = value;
 	}
 
 	public String getName() {
-		return student.getName();
+		// return student.getName();
+		return name;
 	}
 
 	public void setName(String value) {
-		student.setName(value);
+		// student.setName(value);
+		name = value;
 	}
 
 	public String getCourse() {
-		return student.getCourse();
+		// return student.getCourse();
+		return course;
 	}
 
 	public void setCourse(String value) {
-		student.setCourse(value);
+		// student.setCourse(value);
+		course = value;
 	}
 
 	public long getAddressId() {
-		return address.getAddressId();
+		// return address.getAddressId();
+		return this.addressId;
 	}
 
-	public void setAddressId(long value) {
-		address.setAddressId(value);
+	public void setAddressId(long addressId) {
+		// address.setAddressId(value);
+		this.addressId = addressId;
 	}
 
 	public String getStreet() {
-		return address.getStreet();
+		// return address.getStreet();
+		return this.street;
 	}
 
-	public void setStreet(String value) {
-		address.setStreet(value);
+	public void setStreet(String street) {
+		// address.setStreet(value);
+		this.street = street;
 	}
 
 	public String getCity() {
-		return address.getCity();
+		// return address.getCity();
+		return this.city;
 	}
 
-	public void setCity(String value) {
-		address.setCity(value);
+	public void setCity(String city) {
+		// address.setCity(value);
+		this.city = city;
 	}
 
 	public String getState() {
-		return address.getState();
+		// return address.getState();
+		return this.state;
 	}
 
-	public void setState(String value) {
-		address.setState(value);
+	public void setState(String state) {
+		// address.setState(value);
+		this.state = state;
 	}
 
 	public String getZipcode() {
-		return address.getZipcode();
+		// return address.getZipcode();
+		return this.zipcode;
 	}
 
-	public void setZipcode(String value) {
-		address.setZipcode(value);
+	public void setZipcode(String zipcode) {
+		// address.setZipcode(value);
+		this.zipcode = zipcode;
 	}
 
 	public StudentAddress() {
-		this.address = new Address();
-		this.student = new Student();
 	}
 
 }
