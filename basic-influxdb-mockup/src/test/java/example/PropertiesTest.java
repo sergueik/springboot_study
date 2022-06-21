8c26304b9ba3c9218440afb8b2f8d6cab62dfc0e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-06-21 00:32:40 -0400

    intermediate revision - consolidated tests and replaced the line protocol line expression with a better one from test

--- basic-influxdb-mockup/src/test/java/example/PropertiesTest.java
+++ basic-influxdb-mockup/src/test/java/example/PropertiesTest.java
@@ -29,17 +29,6 @@ public class PropertiesTest {
 	public void test1() {
 		payload = "test,foo=bar,baz=bam value=42.0 data=0 1655244130852723";
 		result = utils.parseLineProtocolLine(payload);
-		// NOTE: not the desired behavior
-		assertThat(result, containsString("tag_set=baz=bam"));
-	}
-
-	// TODO: replace the grammar in Utils class with this one
-
-	@Test
-	public void test2() {
-		payload = "test,foo=bar,baz=bam value=42.0 data=0 1655244130852723";
-		grammar = "^([-a-z0-9_]+)((?:,(?:[-a-z0-9A-Z_=\"]+))*) ((?:[-a-z.0-9_]+=[-a-z.0-9_\"]+)(?: [-a-z0-9_]+=[-a-z.0-9_\"]+)*) ([0-9]+)$";
-		result = utils.parseLineProtocolLine(payload, grammar);
 		for (String expression : new String[] { "measurement=test",
 				"tag_set=,foo=bar,baz=bam", "field_set=value=42.0 data=0",
 				"timestamp=1655244130852723" }) {
@@ -48,55 +37,17 @@ public class PropertiesTest {
 	}
 
 	@Test
+	public void test2() {
+		result = utils.resolveTags(",foo=FOO,bar=BAR,baz=BAZ");
+
+		for (String expression : new String[] { "tag_key=foo", "tag_value=FOO",
+				"tag_key=bar", "tag_value=BAR", "tag_key=baz", "tag_value=BAZ" }) {
+			assertThat(result, containsString(expression));
+		}
+	}
+
+	@Test
 	public void test3() {
-		payload = "foo,bar=baz";
-		result = utils.parseLineProtocolLine(payload,
-				"([-a-z0-9_]+)((?:,[-a-z0-9_=\"]+)*)");
-		assertThat(result, containsString("measurement=foo"));
-	}
-
-	@Test
-	public void test4() {
-		payload = "foo value=42";
-		result = utils.parseLineProtocolLine(payload,
-				"([-a-z0-9_]+)((?:,[-a-z0-9_=\"]+)*)");
-		assertThat(result, containsString("measurement=foo"));
-	}
-
-	@Test
-	public void test5() {
-		result = utils.resolveTags(",foo=FOO,bar=BAR,baz=BAZ");
-
-		for (String expression : new String[] { "tag_key=foo", "tag_value=FOO",
-				"tag_key=bar", "tag_value=BAR", "tag_key=baz", "tag_value=BAZ" }) {
-			assertThat(result, containsString(expression));
-		}
-	}
-
-	@Test
-	public void test6() {
-		result = utils.resolveTags(",foo=FOO,bar=BAR,baz=BAZ");
-
-		for (String expression : new String[] { "tag_key=foo", "tag_value=FOO",
-				"tag_key=bar", "tag_value=BAR", "tag_key=baz", "tag_value=BAZ" }) {
-			assertThat(result, containsString(expression));
-		}
-	}
-
-	@Disabled
-	@Test
-	public void test7() {
-		result = utils.parseLineProtocolLine(
-				"testing,appid=BAZ,env=UAT,host=sergueik71,operation=send value=42.0 1655341280");
-
-		for (String expression : new String[] { "measurement=testing",
-				"tag_set=,appid=BAZ,env=UAT,host=sergueik71,operation=send" }) {
-			assertThat(result, containsString(expression));
-		}
-	}
-
-	@Test
-	public void test8() {
 		result = utils.resolveFields("foo=1 bar=2.0 baz=345",
 				"([-a-z0-9_]+)=([-a-zA-Z0-9_]+)");
 		for (String expression : new String[] { "field_key=foo", "field_value=1",
