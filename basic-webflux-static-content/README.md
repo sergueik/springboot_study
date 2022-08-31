ee1047d8326f7f61da5d8ded681caec94de344bb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-09-01 01:38:39 +0200

    initial snapshot - webflux with static content

---
+++ basic-webflux-static-content/README.md
@@ -0,0 +1,6 @@
+### Info
+
+Replica of Springboot Docker [project](https://github.com/daggerok/webflux-static-content) serving static web content with webflux converted to maven
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
---
+++ basic-webflux-static-content/src/main/java/example/Application.java
@@ -0,0 +1,51 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication
+public class Application {
+
+	@Component
+	public class CustomWebFilter implements WebFilter {
+
+		@Override
+		public Mono<Void> filter(final ServerWebExchange exchange,
+				final WebFilterChain chain) {
+
+			final ServerHttpRequest req = exchange.getRequest();
+
+			return req.getURI().getPath().equals("/")
+					? chain.filter(exchange.mutate()
+							.request(req.mutate().path("/index.html").build()).build())
+					: chain.filter(exchange);
+		}
+	}
+
+	@Bean
+	public RouterFunction routerFunction() {
+
+		return route(GET("/api/**"),
+				request -> ok().body(Flux.just("one", "two"), String.class))
+						.andOther(resources("/**", new ClassPathResource("/static")));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
