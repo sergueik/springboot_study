f61e5e2c6cad667d30a4af607a9c35925c3f2b3d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-09-09 01:01:24 +0200

    intermediate revision - added static page and elasticsearch warmup delay note in README

---
+++ basic-elasticrsearch-timeseries/src/main/java/example/controller/AppController.java
@@ -0,0 +1,31 @@
+package example.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Copyright 2022 Serguei Kouzmine
+ */
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class AppController {
+
+	// index page
+	@GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+	@ResponseBody
+	public ResponseEntity<String> index() {
+		String body = "<html>" + "<head>" + "demo app" + "</head>" + "<body>"
+				+ "<a href=\"./insert\"\"> Insert Dummy Metrics</a><br/>"
+				+ "<a href=\"./query\"\">Dump metrics</a><br/>"
+				+ "<a href=\"./metrics\"\">Server inventory (not available yet)</a><br/>"
+				+ "</body>" + "</html>";
+		return ResponseEntity.ok().body(body);
+	}
+
+}
