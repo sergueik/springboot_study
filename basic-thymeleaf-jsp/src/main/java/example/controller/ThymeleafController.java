18f5f5838b3917e4d3159afc73581f54c72e1cb1
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-10-22 00:10:10 +0200

    intermediate revision - replaced  original project page with a Hello World one, added acceptance tests

--- basic-thymeleaf-jsp/src/main/java/example/controller/ThymeleafController.java
+++ basic-thymeleaf-jsp/src/main/java/example/controller/ThymeleafController.java
@@ -6,14 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
 @Controller
 @RequestMapping("/thymeleaf")
 public class ThymeleafController {
 
 	@GetMapping
-	public ModelAndView lista(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+	public ModelAndView hello(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		ModelAndView model = new ModelAndView("/hello.html");
+		model.addObject("name", name);
+		return model;
+	}
+
+	// javax.servlet.ServletException: Could not resolve view with name '/hello'
+	// in servlet with name 'dispatcherServlet'
+	// would also be thrown when resolvers are not configured properly
+	@GetMapping("/misconfigured")
+	public ModelAndView misconfigured(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		ModelAndView model = new ModelAndView("/hello");
 		model.addObject("name", name);
 		return model;
 	}
