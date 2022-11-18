a47082d656ae8493cc2930acd91dcc7562c353d8
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2022-11-19 00:26:58 +0100

    intermediate revision - added dump fields of interest and update example

--- basic-grafana-export-perl/edit.pl
+++ basic-grafana-export-perl/edit.pl
@@ -34,26 +34,55 @@ GetOptions(
     'debug'    => \$debug,
     'dump'     => \$dump
 );
-
+if ( !$inputfile ) {
+    help();
+}
 our $json_pp = JSON::PP->new->ascii->pretty->allow_nonref;
 open my $fh, '<', $inputfile or die "Can't open file $!";
 my $content = do { local $/; <$fh> };
 my $root = eval { $json_pp->decode($content) };
-print STDERR 'Decoded Grafana Dashboard JSON:' . $/ if $debug;
-print STDERR Dumper($root) if $debug;
-my $panel_index = 0;
-my $panel = $root->{'panels'}->[$panel_index]; 
-my $type = $panel->{'type'};
-print STDERR "panel: ${type}", $/;
+if ($dump) {
+    print STDERR 'Grafana Dashboard JSON:' . $/;
+    print STDERR Dumper($root);
+}
+my $panels        = $root->{'panels'};
+my $panel_indexes = 0 .. $#$panels;
+
+foreach my $cnt1 ($panel_indexes) {
+    my $panel = $panels->[$cnt1];
+    print STDERR 'type: ', $panel->{'type'}, $/;
+
+    my $targets        = $panel->{'targets'};
+    my $target_indexes = 0 .. $#$targets;
+
+    foreach my $cnt2 ($target_indexes) {
+        my $target = $targets->[$cnt2];
+        print STDERR 'target: ', $/, 'expr:', $target->{'expr'}, $/;
+        my $datasource = $target->{'datasource'};
+        print STDERR 'datasource: ', $/, 'type:', $datasource->{'type'}, $/;
+    }
+
+}
+
+# NOTE: to update the JSON, need to write the properties of $root
+# which would lead to long chains of key and index references
+
+my $panel_index  = 0;
 my $target_index = 0;
-my $target = $panel->{'targets'}->[$target_index];
-my $expr = $target->{'expr'};
-my $datasource = $target->{'datasource'};
-my $datasource_type = $datasource->{'type'};
-print STDERR "detail: expr: \"${expr}\", datasource: \"${datasource_type}\"", $/;
-# modify values through valid element reference. 
+
+# modify values through valid element reference.
 # This is to avoid using the template
 # which is another valid  option to consider
 $root->{'panels'}->[$panel_index]->{'type'} = 'modified';
-$root->{'panels'}->[$panel_index]->{'targets'}->[$target_index]->{'datasource'}->{'type'} = 'new datasource type';
+$root->{'panels'}->[$panel_index]->{'targets'}->[$target_index]->{'datasource'}
+  ->{'type'} = 'new datasource type';
 print "Result:\n", $json_pp->encode($root);
+
+sub help() {
+    print STDERR <<EOF; exit 0;
+Usage: edit.pl [--debug] [--dump] --input <JSON>
+EOF
+}
+1;
+__END__
+
