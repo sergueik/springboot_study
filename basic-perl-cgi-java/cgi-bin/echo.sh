e3d1546fe6e58ee1afecc0223fc41884ae858ba0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-03-28 01:17:00 +0200

    intermediate revision - replace blunt reading all STDIN with HTTP environment-sawwy load

--- basic-perl-cgi-java/cgi-bin/echo.sh
+++ basic-perl-cgi-java/cgi-bin/echo.sh
@@ -1,9 +1,21 @@
-#!/bin/sh 
-DATA=$(cat -)
+#!/bin/sh
+# based on https://www.unix.com/shell-programming-and-scripting/232805-parsing-http-post-request.html
+# NOTE: the following does not work
+# REQUEST_METHOD=POST CONTEXT_LENGTH=16 echo '{"1234": "5678"}' | ./echo.sh
+# need to export:
+# export REQUEST_METHOD=POST; export CONTEXT_LENGTH=16; echo '{"1234": "5678"}' | ./echo.sh
 # NOTE: on Alpine jq is in /usr/bin
 JQ='/usr/bin/jq'
 if [ ! -z "$OS" ] ; then
   # On Windows assume running from git bash shell and use custom path to the tool
   JQ=/c/tools/jq-win64.exe
 fi
-echo $DATA | $JQ '.'
+
+if  [ "$REQUEST_METHOD" = 'POST' ] ; then
+  if  [ "$CONTEXT_LENGTH" -gt 0 ]; then
+    while read -n $CONTEXT_LENGTH DATA <&0
+    do
+      echo $DATA | $JQ '.'
+    done
+  fi
+fi
