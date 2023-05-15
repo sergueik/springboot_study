86581fa1d2e3c91213e29a186ea634d787a8ca73
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-05-16 01:13:07 +0200

    intermediate revision - replaced hard coded host name of rest backend with usual default "window.location.origin" and variabne name refactoring

--- basic-angularjs-validation/src/main/resources/static/js/script.js
+++ basic-angularjs-validation/src/main/resources/static/js/script.js
@@ -1,7 +1,7 @@
 var app = angular.module('developapp', []);
 
 app.controller('formcontroller', function($scope, $http) {
-    $scope.url = 'http://192.168.0.25:8080/json';
+    $scope.url = window.location.origin + '/json';
     $scope.update = function() {
         console.log('processing');
     }
@@ -10,18 +10,18 @@ app.controller('formcontroller', function($scope, $http) {
 app.directive('ageValidationDirective', function() {
     return {
         require: 'ngModel',
-        link: function(scope, element, attr, mCtrl) {
+        link: function(scope, element, attr, controller) {
             function ageValidation(value) {
                 if (value >= 18) {
                     console.log('valid: ' + value);
-                    mCtrl.$setValidity('charE', true);
+                    controller.$setValidity('charE', true);
                 } else {
                     console.log('invalid: ' + value);
-                    mCtrl.$setValidity('charE', false);
+                    controller.$setValidity('charE', false);
                 }
                 return value;
             }
-            mCtrl.$parsers.push(ageValidation);
+            controller.$parsers.push(ageValidation);
         }
     };
 });
@@ -32,9 +32,12 @@ app.directive('probeValidationDirective', function($http, $q) {
         link: function($scope, element, attrs, ngModel) {
 
             ngModel.$asyncValidators.userAvailable = function(modelValue, viewValue) {
-
                 var userInput = modelValue || viewValue;
-                return $http.get('http://192.168.0.25:8080/json/' + userInput).then(function(response) {
+                if (userInput === undefined) {
+                    return $q.reject('mising user input');
+                }
+                console.log('request: ' + $scope.url + '/' + userInput);
+                return $http.get($scope.url + '/' + userInput).then(function(response) {
                     // console.log('response: '+ JSON.stringify(response));
                     var result = parseInt(response.data.name, 10);
                     console.log('result: ' + result);
@@ -44,7 +47,6 @@ app.directive('probeValidationDirective', function($http, $q) {
                         return true;
                     } else {
                         console.log('invalid: ' + result);
-
                         return $q.reject('invalid: ' + result);
                     }
                 }, function(reason) {
