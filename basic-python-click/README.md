12487e99aa9e2c0c12f85efb4fbcf3cf4ce4c7d3
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-05-17 19:57:37 +0200

    intermediate revision - added "click" decorator exercise

---
+++ basic-python-click/README.md
@@ -0,0 +1,12 @@
+### Info
+
+This dir contains examples from [Python click tutorial](https://zetcode.com/python/click/)
+### Se Also
+
+  * [definitive Guide to Python Click](https://www.assemblyai.com/blog/the-definitive-guide-to-python-click/) - possibly, not the best
+  * https://zetcode.com/python/click/
+  * [building command line applications with Click](https://pymbook.readthedocs.io/en/latest/click.html)
+  * __Working with Databases in Python 3__ [pluralsight training](https://app.pluralsight.com/library/courses/python-3-working-databases) making extensive usage of `click` in practical programs
+
+### Author
+[Serguei Kouzmine](kouzmine_serguei@yahoo.com)
---
+++ basic-python-click/example.py
@@ -0,0 +1,46 @@
+import click
+import json
+from pprint import pprint
+
+# NOTE: cannot use both decorators - will be getting TypeError: Attempted to convert a callback into a command twice.
+# @click.group("cli")
+@click.command()
+@click.pass_context
+@click.argument( 'document', default = 'data.json' )
+@click.argument('value', type = int, default = 0)
+# NOTE: need to match function parameter / argumens. Cannot declare a placeholder parameter), otherwise
+# TypeError: func() got an unexpected keyword argument 'value'
+# def func(ctx, document):
+# def func(ctx, document, _):
+def func(ctx, document, value):
+  # NoneType
+  pprint(type(ctx.obj))
+  with open(document) as _stream:
+    ctx.obj = json.load(_stream)
+    pprint(type(ctx.obj))
+    pprint(ctx.obj)
+
+@click.command()
+@click.argument('values', type = str, nargs = -1)
+def varargs(values):
+  click.echo(*values)
+  # for int values
+  # click.echo(f'The product is {reduce(mul, vals, 1)}')
+
+@click.command()
+@click.option('-s', '--name', prompt = 'Your name argument', help = 'Provide "name" argument')
+# parameter name is fixed
+def named_ars(name):
+  click.echo('name = "{}"'.format(name))
+
+@click.command()
+@click.option('--words', '-ww', multiple = True)
+def multiple_args(words):
+    click.echo('words: {}'.format(','.join(words)))
+# NOTE: example1.py does not exercise group
+if __name__ == '__main__':
+  # NOTE: not passing arguments
+  #  func()
+  # varargs()
+  # named_args()
+  multiple_args()
