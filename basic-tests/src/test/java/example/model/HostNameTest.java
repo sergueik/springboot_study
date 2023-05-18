d55398e9e54a768aee189d697cb47a3cccc2dc11
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-05-19 00:08:41 +0200

    intermediate revision - refactored the tests to avoid using hardcoded prefixes and  hostnames

--- basic-tests/src/test/java/example/model/HostNameTest.java
+++ basic-tests/src/test/java/example/model/HostNameTest.java
@@ -27,8 +27,14 @@ import java.util.stream.Collectors;
 public class HostNameTest {
 	private static Set<String> result;
 
-	private static final List<String> inputs = Arrays
-			.asList(new String[] { "aaa-bbbbb", "cccddddd", "aa-bbb", "aa-ccc", });
+	private static final String suffix = "ys4jaw6dniqc1bmq";
+	private static String prefix;
+
+	private static final List<String> inputs = Arrays.asList(new String[] {
+			"aa-1ibqg2e7aeiqnbcck", "aa-2fg1yh4maaprikjuo", "aaa3wtw4zrn8v2dbtl6s",
+			"aaa4df0iva3sebbkcnn5", "ccc5cwje3bcgz0qy1if9", "ccc6nhau7oktm2qjgmtb",
+			"aaa708b4x9gnpvsxqdhz", "aaa8pdan4aftvho8yrne", "aa-9sthbd3khq4fqltob",
+			"aa-0pjx31tcyuy0ubcvo" });
 	private static String[] checkResults = new String[] { "aaa", "ccc", "aa" };
 
 	// https://www.baeldung.com/junit-assert-exception
@@ -36,27 +42,34 @@ public class HostNameTest {
 	// this is Junit 5
 	@Test
 	public void test2() {
+		prefix = "zzz";
 		List<String> inputs2 = new ArrayList<>();
-		inputs2.addAll(inputs);
-		inputs2.add("zzzzzzzz");
-		result = inputs2.stream().map((String o) -> {
-			return new HostName(o);
-		}).map((HostName o) -> o.getHostPrefix()).collect(Collectors.toSet());
+		for (String exrahost : Arrays
+				.asList(new String[] { String.format("%s%s", prefix, suffix),
+						String.format("%s-%s", prefix, suffix),
+						String.format("%sabc-%s", prefix, suffix) })) {
+			inputs2.clear();
+			inputs2.addAll(inputs);
+			inputs2.add(exrahost);
 
-		// incompatible types: inference variable T has incompatible bounds
-		// equality constraints: java.lang.AssertionError
+			result = inputs2.stream().map((String o) -> {
+				return new HostName(o);
+			}).map((HostName o) -> o.getHostPrefix()).collect(Collectors.toSet());
 
-		Exception exception = assertThrows(Exception.class, () -> {
-			try {
-				assertThat(result, containsInAnyOrder(checkResults));
-				System.err.println("Returned: " + result);
-				System.err.println("Expected: " + Arrays.asList(checkResults));
-			} catch (AssertionError e) {
-				throw new Exception(e.getMessage());
-			}
-		});
+			Exception exception = assertThrows(Exception.class, () -> {
+				try {
+					assertThat(result, containsInAnyOrder(checkResults));
+					System.err.println("Returned: " + result);
+					System.err.println("Expected: " + Arrays.asList(checkResults));
+				} catch (AssertionError e) {
+					throw new Exception(e.getMessage());
+				}
 
-		assertThat(exception.getMessage(), containsString("no match for: \"zzz\""));
+			});
+			assertThat(exception.getMessage(),
+					containsString(String.format("no match for: \"%s\"", prefix)));
+		}
+
 	}
 
 	@Test
@@ -64,7 +77,11 @@ public class HostNameTest {
 
 		List<String> inputs2 = new ArrayList<>();
 		inputs2.addAll(inputs);
-		inputs2.remove("cccddddd");
+		prefix = "aaa";
+		List<String> inputs3 = inputs2.stream()
+				.filter((String o) -> o.startsWith(prefix))
+				.collect(Collectors.toList());
+		inputs2.removeAll(inputs3);
 		result = inputs2.stream().map((String o) -> {
 			return new HostName(o);
 		}).map((HostName o) -> o.getHostPrefix()).collect(Collectors.toSet());
@@ -81,9 +98,10 @@ public class HostNameTest {
 		});
 
 		assertThat(exception.getMessage(),
-				containsString("no item matches: \"ccc\""));
+				containsString(String.format("no item matches: \"%s\"", prefix)));
 	}
 
+	@Disabled("with Junit 5 / Java 11 the message will be \"remove\", but here we get nothing")
 	@Test
 	public void test4() {
 		// inputs.remove("aaa-bbbbb");
@@ -93,7 +111,6 @@ public class HostNameTest {
 					// but not here
 					inputs.remove("aaa-bbbbb");
 				});
-
 		assertThat(exception.getMessage(), nullValue());
 	}
 }
