57fb4733594635919d4ac2f115d745ee1d6c3a0e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-05-24 02:32:44 +0200

    intermediate revision - removed unreliable "prefix" attribute from ConfigurationProperties, added os-specific value set in the property getter and cached

--- basic-multipart-upload/src/main/java/example/property/FileStorageProperties.java
+++ basic-multipart-upload/src/main/java/example/property/FileStorageProperties.java
@@ -1,16 +1,48 @@
 package example.property;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@ConfigurationProperties(prefix = "file")
+// TODO: the prefix no longer work after switching from "file.upload-dir" 
+// @ConfigurationProperties(prefix = "file")
+@ConfigurationProperties
 public class FileStorageProperties {
-	private String uploadDir;
+	private static final Logger logger = LoggerFactory
+			.getLogger(FileStorageProperties.class);
+
+	protected String osName = null;
+
+	@Value("${file.upload-dir-windows}")
+	private String uploadDirWindows;
+	@Value("${file.upload-dir-linux}")
+	private String uploadDirLinux;
+
+	private String uploadDir = null;
 
 	public String getUploadDir() {
+
+		if (uploadDir == null) {
+			osName = getOSName();
+			uploadDir = osName.equals("windows") ? uploadDirWindows : uploadDirLinux;
+		}
+		logger.info("UploadDir: " + uploadDir);
 		return uploadDir;
 	}
 
 	public void setUploadDir(String value) {
 		uploadDir = value;
+	}
+
+	// Utilities
+	public String getOSName() {
+		if (osName == null) {
+			osName = System.getProperty("os.name").toLowerCase();
+			if (osName.startsWith("windows")) {
+				osName = "windows";
+			}
+		}
+		return osName;
 	}
 }
