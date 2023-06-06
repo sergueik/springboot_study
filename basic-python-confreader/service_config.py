40b4bc8ca86ed2c80c4ac500afd6701d05b2badd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-06-07 01:17:14 +0200

    intermediate revision - introduced "process_includes"  and refactored method names, and converted "debug" to a property

--- basic-python-confreader/service_config.py
+++ basic-python-confreader/service_config.py
@@ -6,9 +6,11 @@ from pprint import pprint
 
 class ServiceConfig(object):
 # NOTE: the 'object' super class should be named in all lower case:
-  def __init__(self, filename = None, path = None):
+
+  def __init__(self, filename = None, path = None, debug = False):
      self._file = filename
      self._path = path
+     self._debug = debug
      self._lines = []
      self._config = []
 
@@ -19,6 +21,10 @@ class ServiceConfig(object):
   @property
   def path(self):
     return self._path
+
+  @property
+  def debug(self):
+    return self._debug
 
   @property
   def lines(self):
@@ -36,38 +42,65 @@ class ServiceConfig(object):
   def path(self, value):
     self._path = value
 
-  def process(self):
+  # NOTE: can not decorate setter without decorating @property as well
+  @debug.setter
+  def debug(self, value):
+    self._debug = value
+
+  # methods
+
+  def read_file(self):
     with open(self.path + '/'+ self.filename, mode = 'r') as file:
       for line in file:
         self._lines.append(line.strip())
 
-  def examine(self):
+  def process_includes(self):
+    patt= re.compile('^#include ')
+    # insert
+    scanned = False
+    while not scanned:
+      scanned = True
+      recursion_control_cnt = 0
+      for cnt in range(len(self._lines)):
+
+        line = self._lines[cnt]
+        if line.find(' ') == -1:
+          continue
+
+        if patt.match(line) != None:
+          if self._debug:
+            print('include detected in line {} "{}"'.format(cnt, line))
+          _, filename, *_ = line.split(' ')
+          if os.path.isfile(self.path + '/' + filename ):
+            if self._debug:
+              print('valid include filename: "{}"'.format(filename))
+            s_c = ServiceConfig()
+            s_c.path = self.path
+            s_c.filename = filename
+            s_c.read_file()
+            insert_lines = s_c.lines
+            if self._debug:
+              print('including lines: {}'.format('\n'.join(insert_lines)))
+            scanned = False
+            recursion_control_cnt = recursion_control_cnt + 1
+            if recursion_control_cnt > 5 :
+              # abort recursion which went too far
+              scanned = True
+
+            # NOTE: insert lines is tricky inside the range loop
+            self._lines = self._lines[0:cnt] + insert_lines + self._lines[cnt + 1:]
+            if self._debug:
+              pprint(self._lines)
+
+  def tokenize_lines(self):
     for line in self._lines:
       if line.find(' ') == -1:
         continue
         # alternatively, handle ValueError: not enough values to unpack
 
-      # TODO: rename to arguments
-      command, filename, *variables = line.split(' ')
-      variables = variables + ( [''] * 2 )
-      self._config.append([ command, filename ] + variables[:2] )
-
-    patt= re.compile('^#include')
-    # insert
-    for cnt in range(len(self._config)):
-
-      command, filename, arg1, arg2 = self._config[cnt]
-      if patt.match(command) != None:
-        print('include detected in line {} "{}"'.format(cnt, self._lines[cnt]))
-        if os.path.isfile(self.path + '/' + filename ):
-          print('valid include')
-          s_c = ServiceConfig()
-          s_c.path = self.path
-          s_c.filename = filename
-          s_c.process()
-          insert_lines = s_c.lines
-          print('including lines: {}'.format('\n'.join(insert_lines)))
-          self._lines = self._lines[0:cnt] + insert_lines + self._lines[cnt:]
+      command, filename, *arguments = line.split(' ')
+      arguments = arguments + ( [''] * 2 )
+      self._config.append([ command, filename ] + arguments[:2] )
 
 help_message = 'usage: service_config.py --path <text> -file <text> [--debug]'
 
@@ -91,7 +124,7 @@ if __name__ == '__main__':
 
   global debug
   # TODO:read through envinonment
-  debug = True
+  debug = False
   config_path = os.path.curdir
   config_filename = 'data.conf'
 
@@ -115,11 +148,16 @@ if __name__ == '__main__':
 
   s_c = ServiceConfig()
   s_c.path = config_path
+  s_c.debug = debug
   s_c.filename = config_filename
 
-  s_c.process()
-  pprint(s_c.lines)
+  s_c.read_file()
+  # NOTE: at this point no #include's are processed
+  # pprint(s_c.lines)
 
-  s_c.examine()
+  # TODO: move method invocation into the relevant getters
+  s_c.process_includes()
+  s_c.tokenize_lines()
+
   pprint(s_c.lines)
   pprint(s_c.config)
