7789af42d59d42cc8e83b5c13d5923d40e55ac2b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-04 01:35:00 +0200

    intermediate revision - cleaned up code

--- basic-properties-location/README.md
+++ basic-properties-location/README.md
@@ -1,30 +1,30 @@
 ### Info
 
 ### Usage
+* build application
+
 ```sh
 mvn package
 ```
+* build container
 ```sh
 docker build -f Dockerfile -t basic-location .
 ```
 * run in container (attached)
 ```sh
-docker run -p 8085:8085 basic-location
+docker run --rm -p 8085:8085 basic-location
 ```
 * test 
 ```sh
 curl -s http://192.168.99.100:8085/check
 ```
-Key file path: /work/place/../../root/.keys/key.txt
-Key file path: /work/place/../../root/.keys/key.txt
+```text
 Key file path: /root/.keys/key.txt
 Data: 123
 ```
 ### NOTE:
 
-* special property
-
-`spring.config.location`
+* special property `spring.config.location`  allows redefining `application.properties` -not used in this example
 
 ### Author
 [Serguei Kouzmine](kouzmine_serguei@yahoo.com)
--- basic-properties-location/src/main/java/example/controller/Worker.java
+++ basic-properties-location/src/main/java/example/controller/Worker.java
@@ -2,7 +2,6 @@ package example.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,46 +21,24 @@ public class Worker {
 	Properties properties;
 
 	private final String fileName = "key.txt";
-	private Path path;
-	private Path location;
-	private StringBuilder response;
-	private String filePath = null;
 
 	@GetMapping("/check")
 	public String propertyCheck() {
-		response = new StringBuilder();
-		location = Paths
-				.get(new File(String.join(System.getProperty("file.separator"),
-						Arrays.asList(System.getProperty("user.dir"),
-								properties.getProperty("location"), fileName)))
-										.getAbsolutePath());
-		// does not remove ".." from the path
-		response.append("Key file path: " + location.toString() + "\n");
-
-		path = Paths.get(System.getProperty("user.dir"));
-		location = path.resolve(String.join(System.getProperty("file.separator"),
-				Arrays.asList(properties.getProperty("location"), fileName)));
-
-		// does not remove ".." from the path
-		response.append("Key file path: " + location.toString() + "\n");
+		StringBuilder response = new StringBuilder();
 
 		try {
-			// see also:
-			// https://www.digitalocean.com/community/tutorials/java-file-path-absolute-canonical
-			path = Paths.get(System.getProperty("user.dir"));
-			filePath = new File(path
-					.resolve(String.join(System.getProperty("file.separator"),
-							Arrays.asList(properties.getProperty("location"), fileName)))
-					.toString()).getCanonicalPath();
-			// does not remove ".." from the path
+			String filePath = new File(
+					Paths.get(System.getProperty("user.dir"))
+							.resolve(String.join(System.getProperty("file.separator"),
+									Arrays.asList(properties.getProperty("location", "."),
+											fileName)))
+							.toAbsolutePath().toString()).getCanonicalPath();
 			response.append("Key file path: " + filePath + "\n");
-			response.append("Data: "
-					+ new String(Files.readAllBytes(location), StandardCharsets.UTF_8));
+			response
+					.append("Data: " + new String(Files.readAllBytes(Paths.get(filePath)),
+							StandardCharsets.UTF_8));
 		} catch (IOException e) {
-
-			// does not remove ".." from the path
 			response.append("Key file path: " + "error" + "\n");
-
 		}
 
 		return response.toString();
