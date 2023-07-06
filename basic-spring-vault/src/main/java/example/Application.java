3090dc503c64734bba39d9349541e577cd6f2c5e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-06 04:10:18 +0200

    intermediate revision - added working example, plus vanilla refactoring (contd.)

--- basic-spring-vault/src/main/java/example/Application.java
+++ basic-spring-vault/src/main/java/example/Application.java
@@ -1,16 +1,19 @@
-package com.codeforgeyt.vaultdemomvn;
+package example;
 
-import com.codeforgeyt.vaultdemomvn.configuration.VaultConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import example.configuration.VaultConfiguration;
+
 @SpringBootApplication
-public class VaultDemoMvnApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(VaultDemoMvnApplication.class, args);
-		VaultConfiguration vaultConfiguration = context.getBean(VaultConfiguration.class);
+		ConfigurableApplicationContext context = SpringApplication
+				.run(Application.class, args);
+		VaultConfiguration vaultConfiguration = context
+				.getBean(VaultConfiguration.class);
 		System.out.println("Login: " + vaultConfiguration.getLogin());
 		System.out.println("Password: " + vaultConfiguration.getPassword());
 	}
