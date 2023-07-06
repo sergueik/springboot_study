32260db09f9198d1ce01622c478855aac9e36264
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-07-06 04:10:51 +0200

    inermeiate revision - added standard logging

--- basic-properties-location/src/main/java/example/controller/Worker.java
+++ basic-properties-location/src/main/java/example/controller/Worker.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Properties;
@@ -12,10 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/")
 public class Worker {
+
+	private static final Logger logger = LoggerFactory.getLogger(Worker.class);
 
 	@Autowired
 	Properties properties;
@@ -33,15 +38,24 @@ public class Worker {
 									Arrays.asList(properties.getProperty("location", "."),
 											fileName)))
 							.toAbsolutePath().toString()).getCanonicalPath();
-			response.append("Key file path: " + filePath + "\n");
-			response
-					.append("Data: " + new String(Files.readAllBytes(Paths.get(filePath)),
-							StandardCharsets.UTF_8));
+			logger.info("Read file from: " + filePath);
+			Path path = Paths.get(filePath);
+			if (Files.isReadable(path)) {
+				String data = new String(Files.readAllBytes(Paths.get(filePath)),
+						StandardCharsets.UTF_8);
+				logger.info(String.format("Read  %d bytes", data.length()));
+				response.append("Data: " + data);
+			} else {
+				logger.error("Invalid path to the file: " + filePath);
+				response.append("Data: read error");
+			}
 		} catch (IOException e) {
-			response.append("Key file path: " + "error" + "\n");
+			logger.error("Key file path error" + e.getMessage()	);
+			response.append("Key file path error" + e.getMessage());
 		}
 
 		return response.toString();
 	}
 
 }
+	
