dabe305d84920c0324d9dc828d2ca9e959daa881
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-08-22 01:47:15 +0200

    intermedite revision - attempted to add CORS headers; uncertain wht still failing

--- basic/src/main/java/example/controller/RegexpValidationController.java
+++ basic/src/main/java/example/controller/RegexpValidationController.java
@@ -3,18 +3,23 @@ package example.controller;
  * Copyright 2023 Serguei Kouzmine
  */
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,8 +46,55 @@ public class RegexpValidationController {
 					MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<Map<String, Object>> validate(
 			@RequestParam String expression, @RequestParam Optional<Boolean> debug) {
-		return ResponseEntity.status(HttpStatus.OK)
+
+		// https://www.baeldung.com/spring-response-header
+		// https://stackoverflow.com/questions/31612931/cors-issue-on-localhost-while-calling-rest-service-from-angularjs
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+
+		return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders)
 				.body(service.processExpression(expression));
+	}
+
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.OPTIONS, value = "/validate_cors")
+	public ResponseEntity<String> validateCorsOptions() {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+		responseHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+		responseHeaders.set("Access-Control-Allow-Headers", "Content-Type");
+		responseHeaders.setAccessControlAllowOrigin("*");
+		responseHeaders.setAccessControlAllowHeaders(Arrays.asList("Content-Type"));
+		responseHeaders.setAccessControlAllowMethods(
+				Arrays.asList(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS));
+		log.info("Returning headers: " + responseHeaders);
+		return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders)
+				.body("");
+	}
+
+	@ResponseBody
+	@PostMapping(value = "/validate_cors", consumes = {
+			MediaType.APPLICATION_JSON_VALUE }, produces = {
+					MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<Map<String, Object>> validateCors(
+			@RequestBody Map<String, Object> data) {
+
+		// https://www.baeldung.com/spring-response-header
+		// https://stackoverflow.com/questions/31612931/cors-issue-on-localhost-while-calling-rest-service-from-angularjs
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+		responseHeaders.set("Access-Control-Allow-Origin", "*");
+		responseHeaders.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+		responseHeaders.set("Access-Control-Allow-Headers", "Content-Type");
+		responseHeaders.setAccessControlAllowOrigin("*");
+		responseHeaders.setAccessControlAllowHeaders(Arrays.asList("Content-Type"));
+		responseHeaders.setAccessControlAllowMethods(
+				Arrays.asList(HttpMethod.GET, HttpMethod.POST, HttpMethod.OPTIONS));
+		log.info("Returning headers: " + responseHeaders);
+		// Map<String, Object> data = new HashMap<>();
+		data.put("status", "OK");
+		return ResponseEntity.status(HttpStatus.OK).headers(responseHeaders)
+				.body(data);
 	}
 
 }
