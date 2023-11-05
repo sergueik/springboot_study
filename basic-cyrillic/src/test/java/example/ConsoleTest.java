1bdaeb7d9e60ea2d9fcbae144620083b666a3245
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2023-11-06 00:56:33 +0100

    intermediate revision - switched to junit5

--- basic-cyrillic/src/test/java/example/ConsoleTest.java
+++ basic-cyrillic/src/test/java/example/ConsoleTest.java
@@ -3,18 +3,13 @@ package example;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
-// import org.junit.jupiter.api.Disabled;
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
-import org.junit.Test;
 public class ConsoleTest {
 
 	private static final String message = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ абвгдеёжзийклмнопрстцуфхцчшщъыьэюя 12345";
 
-	// @Disabled("The forked VM terminated without properly saying goodbye")
 	@Test
 	public void test() {
 		String consoleEncoding = System.getProperty("consoleEncoding");
