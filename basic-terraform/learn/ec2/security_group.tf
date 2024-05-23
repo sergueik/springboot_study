c34de454044638142460eec543908b47558a71ee
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-05-23 18:33:37 +0200

    exercises from "Exam Prep: HashiCorp Certified Terraform Associate" coursera training

--- basic-terraform/learn/ec2/security_group.tf
+++ basic-terraform/learn/ec2/security_group.tf
@@ -6,31 +6,27 @@ provider "aws" {
 /* NOTE: 
 Error: Incorrect attribute value type
 
-  on ec2.tf line 13, in resource "aws_security_group" "demo":
-  13:   ingress = {
-  14:    from_port = "443"
-  15:    to_port  = "443"
-  16:    protocol = "TCP"
-  17:    cidr_blocks = [ var.ip ]
-  18:   }
-    +----------------
-    ¦ var.ip will be known only after apply
+  on security_group.tf line 23, in resource "aws_security_group" "demo":
+  23:   ingress = {
+  24:    from_port = "443"
+  25:    to_port  = "443"
+  26:    protocol = "TCP"
+  27:    cidr_blocks = [ var.cidr_blocks ]
+  28:   }
+    +--------------------
+    | var.cidr_blocks will be known only after apply
 
 Inappropriate value for attribute "ingress": set of object required.
 */
-/*
 resource "aws_security_group" "demo" { 
    name = "var-demo"
   ingress = {
    from_port = "443"
    to_port  = "443"
    protocol = "TCP"
-cidr_ipv4         = "0.0.0.0/0"
-#   cidr_blocks      = ["0.0.0.0/0"]
-#   cidr_blocks = [ var.cidr_blocks ]
+   cidr_blocks = [ var.cidr_blocks ]
   }
 }
-*/
 output "test" {
   value = var.cidr_blocks
 }
--- basic-terraform/learn/ec2/variables.tf
+++ basic-terraform/learn/ec2/variables.tf
@@ -1,3 +1,2 @@
 variable "cidr_blocks" {
-  default = "202.173.127.130/32"
 }
