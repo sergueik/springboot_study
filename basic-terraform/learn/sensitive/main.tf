97a3506c9b3110b095b27a6a8a0ba82728c12222
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-06-04 13:21:40 -0400

    intermediate revision - addded another "Sensitive" attr. example and bumped the required_Version" of terraform

--- basic-terraform/learn/sensitive/main.tf
+++ basic-terraform/learn/sensitive/main.tf
@@ -1,4 +1,4 @@
 terraform {
-  required_version = "1.1.5"
+  required_version = "~> 1.7.1"
 }
 
--- basic-terraform/learn/sensitive/outputs.tf
+++ basic-terraform/learn/sensitive/outputs.tf
@@ -1,8 +1,15 @@
+// see also: https://developer.hashicorp.com/terraform/tutorials/configuration-language/sensitive-variables
 
 output "secret" {
   value = var.secret
   // NOTE: when sensitive param is not provided, plan ends with the 
   // Error: Output refers to sensitive values
-  // However, terraform.tfstate will contain the inmasked value
+  // However, terraform.tfstate will contain the un-masked value
   sensitive = true
+}
+
+output "db_connect_string" {
+  description = "database connection string"
+  value       = "Server=server; Database=database; Uid=user; Pwd=${var.secret}"
+  sensitive   = true
 }
