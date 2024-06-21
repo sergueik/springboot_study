f7b75463224e942b04b64a2118dd94f656469697
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-06-22 00:40:19 +0200

    intermediate revision - added longest palindrom exercise

---
+++ basic-codility/src/test/java/example/LongestPalindromeTest.java
@@ -0,0 +1,178 @@
+package example;
+/**
+ * Copyright 2024 Serguei Kouzmine
+ */
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Stack;
+import java.util.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class LongestPalindromeTest {
+
+	@Test
+	public void test1() {
+		String data = "abcba";
+		boolean res = isPalindrome(data);
+		assertTrue(res);
+	}
+
+	@Test
+	public void test2() {
+		String data = "abccba";
+		boolean res = isPalindrome(data);
+		assertTrue(res);
+	}
+
+	// @Test
+	public void test3() {
+		String data = "abcda";
+		boolean res = isPalindrome(data);
+		assertFalse(res);
+
+	}
+
+	// @Test
+	public void test4() {
+		String data = "abda";
+		boolean res = isPalindrome(data);
+		assertFalse(res);
+
+	}
+
+	// @Ignore
+	@Test
+	public void test5() {
+		String data = "abcba";
+		boolean res = isPalindrome2(data);
+		assertTrue(res);
+	}
+
+	// @Ignore
+	@Test
+	public void test6() {
+		String data = "abccba";
+		boolean res = isPalindrome2(data);
+		assertTrue(res);
+	}
+
+	@Test
+	public void test7() {
+		String data = "abcda";
+		boolean res = isPalindrome2(data);
+		assertFalse(res);
+
+	}
+
+	@Test
+	public void test8() {
+		String data = "abda";
+		boolean res = isPalindrome2(data);
+		assertFalse(res);
+
+	}
+
+	// @Ignore
+	@Test
+	public void test10() {
+		String data = "I was driving a racecar with my mom at noon and saw the speed radar at level 0 of civic lane.";
+		String result = longestPalindromeWord(data);
+		System.err.println("Result: " + result);
+		assertThat(result, is("racecar"));
+		assertTrue(result.equals("racecar"));
+
+	}
+
+	private boolean isPalindrome(String data) {
+		String reverse = new StringBuilder(data).reverse().toString();
+		for (int i = 0; i != data.length() / 2; i++) {
+			if (data.charAt(i) != reverse.charAt(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private String longestPalindromeWord(String data) {
+		String[] words = data.split(" ");
+		Map<String, Integer> results = new HashMap<>();
+		String result = null;
+		int maxlength = 0;
+		for (int i = 0; i != words.length - 1; i++) {
+			if (isPalindrome(words[i])) {
+				results.put(words[i], words[i].length());
+				if (words[i].length() > maxlength) {
+					maxlength = words[i].length();
+					result = words[i];
+				}
+			}
+		}
+		return result;
+	}
+
+	private boolean isPalindrome2(String data) {
+
+		char[] letters = data.toCharArray();
+		for (int i = 0; i != (letters.length) / 2; i++) {
+			int j = letters.length - 1 - i;
+			char ch1 = letters[i];
+			char ch2 = letters[j];
+			System.err.println(String.format("Scanning %c %c", ch1, ch2));
+			if (ch1 != ch2) {
+				System.err.println("false");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private String badLongestPalindrome(String data) {
+
+		String[] words = data.split(" ");
+		char[] letters = data.toCharArray();
+		Map<Character, Integer> positions = new HashMap<>();
+		int i = 0;
+		String s1 = "";
+		String s2 = "";
+		String result = "";
+		// List<Character> result = new ArrayList<>();
+		int j = letters.length - 1;
+		while (i != letters.length) {
+			while (j != 0 && j > i) {
+				char ch1 = letters[i];
+				char ch2 = letters[j];
+				// if (ch1 == ch1) {
+				if (ch1 == ch1) {
+					s1 = String.format("%s%c", s1, ch1);
+					s2 = String.format("%c%s", ch2, s2);
+					// result = ch1 + s1 + s2 + ch2;
+					//
+				} else {
+					s1 = "";
+					s2 = "";
+				}
+				if (i == j) {
+					result = String.format("%s%s", s1, s2);
+					break;
+				}
+				j--;
+			}
+			i++;
+		}
+		// positions.put(ch1, i);
+		return "";
+	}
+}
