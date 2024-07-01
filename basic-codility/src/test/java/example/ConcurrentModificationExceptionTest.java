d4545cf8cb66473e5ebd01ec658e3ed655dda4d0
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-07-02 01:46:20 +0200

    intermediate revision - added few more SynchronizedMap / ConcurrentHashMap examples

--- basic-codility/src/test/java/example/ConcurrentModificationExceptionTest.java
+++ basic-codility/src/test/java/example/ConcurrentModificationExceptionTest.java
@@ -3,17 +3,20 @@ package example;
  * Copyright 2024 Serguei Kouzmine
  */
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 
@@ -33,6 +36,7 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasKey;
 
 // see also: https://www.baeldung.com/java-concurrentmodificationexception
+// 
 public class ConcurrentModificationExceptionTest {
 
 	private boolean debug = true;
@@ -58,10 +62,31 @@ public class ConcurrentModificationExceptionTest {
 		}
 	}
 
+	// origin:
+	// https://www.baeldung.com/java-synchronizedmap-vs-concurrenthashmap
+	@Test(expected = ConcurrentModificationException.class)
+	public void test2() {
+		data = new HashMap<>();
+		data.put(1, new Object());
+		data.put(2, new Object());
+		Map<Integer, Object> synchronizedMap = Collections.synchronizedMap(data);
+		Iterator<Entry<Integer, Object>> iterator = synchronizedMap.entrySet().iterator();
+		while (iterator.hasNext()) {
+			try {
+				synchronizedMap.put(3, new Object());
+
+				iterator.next();
+			} catch (ConcurrentModificationException e) {
+				System.err.println("Exception " + e.toString());
+				throw e;
+			}
+		}
+	}
+
 	@Test(/* expected = ConcurrentModificationException.class */)
 	// java.lang.AssertionError: Expected exception:
 	// java.util.ConcurrentModificationException
-	public void test2() throws InterruptedException {
+	public void test3() throws InterruptedException {
 
 		data = new ConcurrentHashMap<>();
 
@@ -74,5 +99,18 @@ public class ConcurrentModificationExceptionTest {
 			data.remove(i);
 		}
 		assertThat(data.keySet().size(), is(new Integer(2)));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void test4() {
+		Map<String, Integer> map = new ConcurrentHashMap<>();
+		map.put(null, 1);
+	}
+
+	@Test
+	public void test5() {
+		Map<String, Integer> map = Collections.synchronizedMap(new HashMap<>());
+		map.put(null, 1);
+		assertTrue(map.get(null).equals(1));
 	}
 }
