41ebbdd99e913c8dd708dfbb332fe1c25375c5e9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-09-05 19:36:26 -0400

    initial exersice envieronment 2

--- basic-terraform/gcp/main.tf
+++ basic-terraform/gcp/main.tf
@@ -32,8 +32,37 @@ provider "google" {
 //}
 // cannot destroy
 // https://stackoverflow.com/questions/56039142/enable-apis-using-serviceusage-api-with-a-service-account
-
-resource "google_storage_bucket" "spheric-alcove-430818-f9-bucket" {
+// https://stackoverflow.com/questions/56039142/enable-apis-using-serviceusage-api-with-a-service-account
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket#example-usage---network-basic
+resource "google_storage_bucket" "bucket" {
   name     = "spheric-alcove-430818-f9-bucket"
   location = "us-central1"
+  force_destroy = true
+  uniform_bucket_level_access = false
+  public_access_prevention = "inherited"
 } 
+
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_object#example-usage---network-basic
+resource "google_storage_bucket_object" "picture" {
+  name   = "butterfly01"
+  source = "a.jpg"
+  bucket = google_storage_bucket.bucket.id
+}
+
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_object_access_control#example-usage---network-basic
+
+
+resource "google_storage_object_access_control" "public_rule" {
+  object = google_storage_bucket_object.picture.name
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+//  Error:  
+// Error creating ObjectAccessControl: googleapi: Error 412: The member bindings allUsers and allAuthenticatedUsers are not allowed since public access prevention is enforced., conditionNotMet 
+
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network#example-usage---network-basic
+// resource "google_compute_network" "vpc_network" {
+//  name = "vpc-network"
+//
+//}
