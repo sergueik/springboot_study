eb8762fa78b0077f4e1a8674d9b559ba42db945b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-09-05 20:25:25 -0400

    initial exersice envieronment 3

--- basic-terraform/gcp/main.tf
+++ basic-terraform/gcp/main.tf
@@ -26,43 +26,49 @@ provider "google" {
   credentials = file("keys.json")
 }
 
-// resource "google_project_service" "project" {
-// project = "spheric-alcove-430818-f9"
-// service =    "storage.googleapis.com"
-//}
-// cannot destroy
-// https://stackoverflow.com/questions/56039142/enable-apis-using-serviceusage-api-with-a-service-account
-// https://stackoverflow.com/questions/56039142/enable-apis-using-serviceusage-api-with-a-service-account
-// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket#example-usage---network-basic
-resource "google_storage_bucket" "bucket" {
-  name     = "spheric-alcove-430818-f9-bucket"
-  location = "us-central1"
-  force_destroy = true
-  uniform_bucket_level_access = false
-  public_access_prevention = "inherited"
-} 
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#example-usage
 
-// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket_object#example-usage---network-basic
-resource "google_storage_bucket_object" "picture" {
-  name   = "butterfly01"
-  source = "a.jpg"
-  bucket = google_storage_bucket.bucket.id
+resource "google_service_account" "default" {
+  account_id   = "spheric-alcove-sa"
+  display_name = "Custom SA for VM Instance"
 }
 
-// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_object_access_control#example-usage---network-basic
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
 
+  tags = ["web"]
 
-resource "google_storage_object_access_control" "public_rule" {
-  object = google_storage_bucket_object.picture.name
-  bucket = google_storage_bucket.bucket.name
-  role   = "READER"
-  entity = "allUsers"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+scheduling { 
+preemptible = true
+automatic_restart = false
+}  
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    // allow full access to all Cloud APIs
+    //  https://cloud.google.com/sdk/gcloud/reference/alpha/compute/instances/set-scopes#--scopes
+    scopes = ["cloud-platform"]
+  }
 }
-//  Error:  
-// Error creating ObjectAccessControl: googleapi: Error 412: The member bindings allUsers and allAuthenticatedUsers are not allowed since public access prevention is enforced., conditionNotMet 
-
-// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_network#example-usage---network-basic
-// resource "google_compute_network" "vpc_network" {
-//  name = "vpc-network"
-//
-//}
