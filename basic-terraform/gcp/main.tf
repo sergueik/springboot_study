893b3218a3e0d6e63063948ed6d1e5ba0e67a691
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-09-05 21:25:27 -0400

    initial exersice envieronment 4

--- basic-terraform/gcp/main.tf
+++ basic-terraform/gcp/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "4.45.0"
+      // version = "~>6.1.0"
 
     }
 
@@ -26,56 +26,52 @@ provider "google" {
   credentials = file("keys.json")
 }
 
-// https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#example-usage
-
-resource "google_service_account" "default" {
-  account_id   = "spheric-alcove-sa"
-  display_name = "Custom SA for VM Instance"
-}
-
-resource "google_compute_instance" "default" {
-  name         = "my-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-c"
-
-  tags = ["web"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
-  scheduling {
-    preemptible       = true
-    automatic_restart = false
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-  //  https://cloud.google.com/compute/docs/access/create-enable-service-accounts-for-instances
-  //
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email = google_service_account.default.email
-    // allow full access to all Cloud APIs
-    //  https://cloud.google.com/sdk/gcloud/reference/alpha/compute/instances/set-scopes#--scopes
-    scopes = ["cloud-platform"]
-  }
-}
-
-
+// https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/service_account#argument-reference
 data "google_service_account" "exercise" {
-    account_id = "terraform-with-gcp@spheric-alcove-430818-f9.iam.gserviceaccount.com"
-    project     = "spheric-alcove-430818-f9"
+  account_id = "terraform-with-gcp@spheric-alcove-430818-f9.iam.gserviceaccount.com"
+  project    = "spheric-alcove-430818-f9"
+}
+
+data "google_service_account_iam_policy" "exercise" {
+  service_account_id = data.google_service_account.exercise.name
+}
+
+output "service_account_json" {
+  value = "${jsonencode(data.google_service_account.exercise)}"
+}
+
+output "service_account" {
+  value = data.google_service_account.exercise
+}
+
+output "service_account_iam_policy" {
+  value = data.google_service_account_iam_policy.exercise
+}
+// https://stackoverflow.com/questions/47006062/how-do-i-list-the-roles-associated-with-a-gcp-service-account
+/*
+
+gcloud projects get-iam-policy "spheric-alcove-430818-f9" --flatten="bindings[].members"   --format="table(bindings.role)"   --filter="bindings.members:terraform-with-gcp@spheric-alcove-430818-f9.iam.gserviceaccount.com"
+
+ROLE
+roles/compute.admin
+roles/iam.serviceAccountAdmin
+roles/iam.serviceAccountUser
+roles/servicemanagement.admin
+roles/serviceusage.serviceUsageAdmin
+roles/storage.admin
+*/
+// TODO: https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/iam_policy
+data "google_iam_policy" "report" {
+  binding {
+    role = "roles/compute.admin"
+
+    members = [
+      "serviceAccount:terraform-with-gcp@spheric-alcove-430818-f9.iam.gserviceaccount.com",
+    ]
+  }
+}
+
+output "xxx" {
+value = data.google_iam_policy.report
+
 }
