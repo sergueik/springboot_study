21e1dd4b3facf7ffbecea7a5ca1c31831aca090a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-09-09 17:23:51 -0400

    added exercise

--- basic-terraform/gcp/data-sources/main.tf
+++ basic-terraform/gcp/data-sources/main.tf
@@ -26,9 +26,60 @@ provider "google" {
   credentials = file("../keys.json")
 }
 // https://github.com/PacktPublishing/Terraform-for-Google-Cloud-Essential-Guide/tree/main/chap03/data-source
-data "google_compute_instance" "this" {
-  name = "instance-1"
+
+data "google_compute_zones" "available" {
 }
-output "ip-address" {
-  value = format("IP address of existing server: %s", data.google_compute_instance.this.network_interface[0].access_config[0].nat_ip)
+
+resource "google_compute_instance" "this" {
+  count        = var.instance_number
+  name         = "${var.server_name}-${count.index}"
+  machine_type = var.machine_type
+  zone         = data.google_compute_zones.available.names[count.index % length(data.google_compute_zones.available.names)]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
+
+variable "region" {
+  type        = string
+  description = "Default Region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  description = "Default Zone"
+  default     = "us-central1-b"
+}
+
+variable "server_name" {
+  type        = string
+  description = "Name of Webserver"
+  default     = "webserver"
+}
+
+variable "machine_type" {
+  type        = string
+  description = "Machine Type"
+  default     = "e2-micro"
+}
+
+variable "instance_number" {
+  type    = number
+  default = 2
+}
+
+variable "network_name" {
+  type    = string
+  default = "default"
 }
