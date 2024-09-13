a4acc3a5dfcfda3a922e9821f74b680b1f726d8e
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2024-09-13 13:56:07 -0400

    updated exercise, not working filrewall

--- basic-terraform/gcp/mod/modules/server/main.tf
+++ basic-terraform/gcp/mod/modules/server/main.tf
@@ -3,24 +3,38 @@ resource "google_compute_address" "static" {
   name  = "${var.name}-static-ipv4"
 }
 
-
 resource "google_compute_firewall" "allow_http" {
+  // count   = var.has_static_ip ? 1 : 0
   name    = "allow-http-rule"
   network = "default"
   allow {
     ports    = ["80"]
     protocol = "tcp"
-  } 
-  source_ranges = [ "0.0.0.0/0" ]
-  target_tags = ["http-server"]
-  priority    = 1000
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+  priority      = 1000
 
 }
+/*
+output "name" {
+  value = google_compute_address.static.address
+}
 
+*/
 resource "google_compute_instance" "this" {
   name         = var.name
   zone         = var.zone
   machine_type = "e2-micro"
+
+  // https://cloud.google.com/compute/docs/instances/create-use-spot#terraform
+  scheduling {
+    preemptible                 = "true"
+    provisioning_model          = "SPOT"
+    automatic_restart           = "false"
+    instance_termination_action = "STOP"
+  }
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -28,16 +42,19 @@ resource "google_compute_instance" "this" {
   }
   network_interface {
     network = "default"
+
     dynamic "access_config" {
 
       for_each = google_compute_address.static
       content {
         // https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/compute_address#addres
+
         nat_ip = access_config.value["address"]
       }
     }
 
   }
-  metadata_startup_script =  file(join("/", [path.module , "startup.sh"] ))
-  tags = ["http-server"]
+
+  metadata_startup_script = file(join("/", [path.module, "./startup.sh"]))
+  tags                    = ["http-server"]
 }
