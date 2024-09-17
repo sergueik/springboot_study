22f2cc984d8de048c95217bceab985aa2e53ed2a
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-09-17 13:56:53 -0400

    added workspace exercise

---
+++ basic-terraform/learn/hcp_workpaces/main.tf
@@ -0,0 +1,31 @@
+	
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "ubuntu" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
