93508a2128a79c837a5681388d17c5f06040a555
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-10-09 17:23:18 -0400

    added ordering

--- basic-terraform/learn/null-resource/main.tf
+++ basic-terraform/learn/null-resource/main.tf
@@ -6,27 +6,27 @@ resource "null_resource" "example" {
     always = timestamp()
   }
   provisioner "local-exec" {
-    command = "echo 234 > ${path.module}/a.txt"
-
+    command = "echo ${local.jsondata} > ${path.module}/a.txt"
   }
-
-
 }
 
-
+// shows old contents of the file
 
 locals {
-testdata = file("${path.module}/a.txt")
+  jsondata = "{\"a\": 234 }"
+  testdata = file("${path.module}/a.txt")
 }
 
 output "result" {
-value = local.testdata 
+  value = local.testdata
 }
 
 data "local_file" "a" {
-  filename = "${path.module}/a.txt"
+  filename   = "${path.module}/a.txt"
+  depends_on = [null_resource.example]
 }
 
+// https://registry.terraform.io/providers/hashicorp/local/latest/docs/data-sources/file
 output "result2" {
-value =  data.local_file.a.content
+  value = data.local_file.a.content
 }
