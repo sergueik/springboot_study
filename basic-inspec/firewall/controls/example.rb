6b3c1b292acbac34a4f0ef1f571793703e4bdf55
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2024-10-28 14:38:50 -0400

    chatgpt  example, working

--- basic-inspec/firewall/controls/example.rb
+++ basic-inspec/firewall/controls/example.rb
@@ -3,22 +3,28 @@ control 'google-compute-firewall-allow-http' do
   title 'Ensure HTTP and HTTPS traffic is allowed for the specified firewall rule'
   desc 'Check that the firewall rule allows HTTP (port 80) and HTTPS (port 443) ingress traffic over TCP.'
 
-  # Replace these values with your actual project ID and firewall rule name
   firewall_name = 'allow-http-https'
   project_id = 'spheric-alcove-430818-f9'
 
   describe google_compute_firewall(project: project_id, name: firewall_name) do
     it { should exist }
+
+   
     its('direction') { should cmp 'INGRESS' }
     its('disabled') { should cmp false }
     its('source_ranges') { should include '0.0.0.0/0' }
     its('target_tags') { should include 'http-server' }
 
-    # Check that the firewall rule allows TCP traffic on ports 80 and 443
+    # Check if there is an 'allowed' rule with protocol 'tcp' and ports '80' and '443'
     it 'should allow TCP traffic on ports 80 and 443' do
-      expect(subject.allowed).not_to be_empty
-      expect(subject.allowed.map { |rule| rule['protocol'] }).to include 'tcp'
-      expect(subject.allowed.map { |rule| rule['ports'] }.flatten).to include('80', '443')
+      tcp_rule = subject.allowed.find { |rule| rule.ip_protocol == 'tcp' }
+  print "Allowed Rules: ", subject.allowed
+puts subject.allowed.inspect
+      expect(tcp_rule).not_to be_nil
+      expect(tcp_rule.ports).to include('80', '443')
     end
-  end
 end
+end
+
+
+
