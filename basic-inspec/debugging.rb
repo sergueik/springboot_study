5ee9406f5fd6519d7c84fe25d9b2749bf8be3d58
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2024-11-01 14:01:59 -0400

    example debugging from the test, 2

--- basic-inspec/debugging.rb
+++ basic-inspec/debugging.rb
@@ -1,20 +1,24 @@
-# Assuming you are testing an InSpec resource, you can print its methods and properties by calling the public_methods or instance_variables on the resource.
 control 'example' do
   describe google_compute_instance(project: 'my_project', zone: 'us-central1-a', name: 'my-instance') do
     it 'prints the public methods and properties of the resource' do
       # Print all public methods
       puts "Public Methods: #{subject.public_methods(false)}"
-      
-      # Print instance variables (properties)
-      puts "Properties: #{subject.instance_variables}"
+
+      # Print instance variables (properties) and their values
+      puts "Properties:"
+      subject.instance_variables.each do |var|
+        puts "#{var}: #{subject.instance_variable_get(var)}"
+      end
 
       # Print method values (fetch dynamically)
       subject.public_methods(false).each do |method|
         puts "#{method}: #{subject.send(method)}" if subject.respond_to?(method)
       end
     end
-      it 'prints all public methods' do
-      puts subject.methods.sort
-    end
 
-  end      
+    # A normal test example
+    it { should exist }
+    its('status') { should cmp 'RUNNING' }
+  end
+end
+
