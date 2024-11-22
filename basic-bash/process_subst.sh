c936f1cb17f61c9baef6331034a6eed42ecaadc3
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2024-11-22 16:03:03 -0500

    exercise

--- basic-bash/process_subst.sh
+++ basic-bash/process_subst.sh
@@ -16,3 +16,13 @@ IFS=','
 	read col1 col2 col3; do
   printf "col1:%s col2:%s col3:%s\n" "$col1" "$col2" "$col3"
 done < <(echo -e "a1,b1,c1\na2,b2,c2")
+
+# https://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+
+
+while IFS=',' read -ra C; do
+	echo "${C[*]}"
+  for i in "${!C[@]}"; do
+  printf "col%s:%s\n" "$i" "${C[$i]}"
+  done
+done < <(echo -e "a1,b1,c1\na2,b2,c2")
