f096bbdad2bcbe67feece3b0990b59b6d6b7aa7c
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-07-22 17:59:30 -0400

    intermediate revision - idiomatic refactoring

--- basic-prometheus-file_sd_config/myapp.pl
+++ basic-prometheus-file_sd_config/myapp.pl
@@ -4,42 +4,45 @@ use strict;
 
 BEGIN {
     # NOTE: use environment to construct @INC
-    use constant HOME    => (
+    use constant HOME => (
         do { $_ = $ENV{HOME}; /\/([^\/]+)$/ }
     );
     use constant SCRIPT_DIR => (
         do { my $s = `dirname $0`; chomp $s; $s }
     );
-        unshift( @INC, SCRIPT_DIR . '/lib' );
-        unshift( @INC, `pwd` . '/lib' );
+    unshift( @INC, SCRIPT_DIR . '/lib' );
+    unshift( @INC, `pwd` . '/lib' );
 }
 
 use JSON::PP;
 use Mojolicious::Lite;
 
 get '/data' => sub {
-  my $c  = shift;
-  my $ts = $c->param('ts');
-  app->log->info("Received timestamp: ". ( $ts // " data missing"));
-  # || checks if the value is truthy (e.g., 0 is false).
-  # // checks if the value is defined (allows 0 to pass through).
-  # // is available in Perl 5.10
+    my $c  = shift;
+    my $ts = $c->param('ts');
+    app->log->info( "Received timestamp: " . ( $ts // " data missing" ) );
 
+    # || checks if the value is truthy (e.g., 0 is false).
+    # // checks if the value is defined (allows 0 to pass through).
+    # // is available in Perl 5.10
 
-   my $render_data =
-    !defined $ts
-      ? { status => 400, json => { error => "Missing required 'ts' query parameter" } }
-    : $ts !~ /^\d{10}$/
-      ? { status => 405, json => { error => "'ts' must be a valid Unix timestamp in seconds" } }
-    : { json => { metric_value => 42, timestamp => $ts } };
+    my %render_data =
+      !defined $ts
+      ? (
+        status => 400,
+        json   => { error => "Missing required 'ts' query parameter" }
+      )
+      : $ts !~ /^\d{10}$/ ? (
+        status => 405,
+        json   => { error => "'ts' must be a valid Unix timestamp in seconds" }
+      )
+      : ( json => { metric_value => 42, timestamp => $ts } );
 
-
-$c->render(%$render_data);
+    $c->render(%render_data);
 
 };
 
 app->start;
 
 1;
-
 
