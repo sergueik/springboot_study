ea41942f7e7593beda65f1568edf7307b3f0a6ba
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2025-07-24 19:01:42 -0400

    fixed code error with "httpx.utils"

--- basic-python-vscode-debug/app/main.py
+++ basic-python-vscode-debug/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from datetime import datetime
 import httpx
 import os
 
@@ -7,14 +8,18 @@ app = FastAPI()
 TARGET_HOST = os.getenv("TARGET_HOST", "app")
 TARGET_PORT = os.getenv("TARGET_PORT", "80")
 
+@app.get("/")
+async def home():
+    hello_world = "Hello World!"
+    return hello_world
+
 @app.get("/data")
 async def proxy_data(request: Request, ts: str = None):
-    # Use ts from query param if given, else generate current unix timestamp
-    ts_value = ts or str(int(httpx.utils.datetime.utcnow().timestamp()))
+    # Use ts from query param if given, fallback to current unix timestamp
+    ts_value = ts or str(int(datetime.utcnow().timestamp()))
     url = f"http://{TARGET_HOST}:{TARGET_PORT}/data?ts={ts_value}"
     async with httpx.AsyncClient() as client:
         resp = await client.get(url)
         return resp.json()
 
-app = FastAPI()
 
