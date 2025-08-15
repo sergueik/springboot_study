04038b9ae03bc5ccc3f567c392b136ef4135ada5
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-08-15 14:24:51 -0400

    intermediate revision - added vault backed endpoint

--- basic-pbkdf2-csharp/Server/Server.cs
+++ basic-pbkdf2-csharp/Server/Server.cs
@@ -5,38 +5,76 @@ using Microsoft.AspNetCore.Http;
 using System;
 using Utils;
 
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
+
 // NOTE: avoid .NET 6 minimal APIs encouraged top-level statements code layout
 //
-namespace CryptoService {
+namespace CryptoService
+{
 
-    public class Server {
+	public class Server
+	{
 		private static String saltString = null;
 		private static String password = "password";
 		private static bool debug = false;
 		public static bool Debug { set { debug = value; } }
 		private static bool strong = false;
-        public static void Main(string[] args) {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			var app = builder.Build();
 
-            app.Use(async (context, next) => {
-                Console.WriteLine($"{context.Request.Method} {context.Request.Path}");
-                await next();
-            });
+			app.Use(async (context, next) => {
+				Console.WriteLine($"{context.Request.Method} {context.Request.Path}");
+				await next();
+			});
+			app.MapGet("/password", async () => {
 
-            app.MapGet("/encrypt", (string value, string password) => {
-			    var result = Encrypt(value, password,saltString);
-                return Results.Json(new { result = result, original = value });
-            });
+				var result = "";
+				string vaultUri = "http://127.0.0.1:8200";
 
-            app.MapGet("/decrypt", (string value, string password) => {
-			    var result = Decrypt(value, password);
-                return Results.Json(new { result = result, original = value });
-            });
+				string vaultToken = "YOUR_VAULT_TOKEN"; 
 
-            app.Run();
-        }
-		public static string Decrypt(String payloadString, String passwordString) {
+				string secretPath = "secret/data/my-application/config"; 
+
+				try {
+					var vaultClientSettings = new VaultClientSettings(vaultUri, new TokenAuthMethodInfo(vaultToken));
+
+					IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+					Secret<SecretData> secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: secretPath);
+
+					if (secret?.Data?.Data != null) {
+						foreach (var entry in secret.Data.Data) {
+							Console.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+						}
+						result = secret.Data.Data["password"] as string;
+					} else {
+						Console.WriteLine($"Secret at path '{secretPath}' not found or has no data.");
+					}
+				} catch (Exception ex) {
+					Console.WriteLine($"An error occurred: {ex.Message}");
+				}
+				return Results.Json(new { result = result});
+			});
+
+
+			app.MapGet("/encrypt", (string value, string password) => {
+				var result = Encrypt(value, password, saltString);
+				return Results.Json(new { result = result, original = value });
+			});
+
+			app.MapGet("/decrypt", (string value, string password) => {
+				var result = Decrypt(value, password);
+				return Results.Json(new { result = result, original = value });
+			});
+
+			app.Run();
+		}
+		public static string Decrypt(String payloadString, String passwordString)
+		{
 			var aes = new AES();
 			aes.Strong = strong;
 			aes.Debug = debug;
@@ -45,7 +83,8 @@ namespace CryptoService {
 			return decrypted;
 		}
 
-		public static string Encrypt(String payloadString, String passwordString, String saltString) {
+		public static string Encrypt(String payloadString, String passwordString, String saltString)
+		{
 			var aes = new AES();
 			aes.Strong = strong;
 			aes.Debug = debug;
@@ -53,6 +92,6 @@ namespace CryptoService {
 			Console.Error.WriteLine((debug) ? "encrypted: " + encrypted : encrypted);
 			return encrypted;
 		}
-    }
+	}
 }
 
