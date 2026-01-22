c08ac67e565a514a2f0e3b8ad41b0131a90defe9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-01-22 00:07:29 -0500

    intermediate revision - switched to "github" releases of JRrecord/cb2xml

--- basic-jrecord-dummydata/setup_maven_env.sh
+++
@@ -1,9 +0,0 @@
-i#!/bin/bash
-# Step 1: Set Maven env for a private repository
-export MAVEN_LOCAL_REPO="$HOME/.m2/private-repo"
-mkdir -p "$MAVEN_LOCAL_REPO"
-
-export MAVEN_OPTS="-Dmaven.repo.local=$MAVEN_LOCAL_REPO"
-
-echo "Maven local repo set to $MAVEN_LOCAL_REPO"
-
