2c08b53400b2457912e9d2c2f2b253dfe69dabf9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-02-24 21:15:15 -0500

    intermediate revision - added code to reproduce the issue observed in the "basic-parallel-filechunk" but it does not hit the eror in this project

--- basic-jrecord-meteredreader/src/main/java/example/utils/CopybookBatchReader.java
+++ basic-jrecord-meteredreader/src/main/java/example/utils/CopybookBatchReader.java
@@ -6,6 +6,9 @@ import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.JRecord.JRecordInterface1;
 import net.sf.JRecord.Common.FieldDetail;
 import net.sf.JRecord.Common.IFileStructureConstants;
@@ -18,9 +21,11 @@ import net.sf.JRecord.IO.AbstractLineReader;
 import net.sf.JRecord.def.IO.builders.ICobolIOBuilder;
 
 public class CopybookBatchReader implements Closeable {
+	private static final Logger log = LoggerFactory.getLogger(CopybookBatchReader.class);
 
 	private AbstractLineReader reader;
 	private LayoutDetail layout;
+	private long recordSize;
 
 	public CopybookBatchReader(Path copybookFile, Path dataFile, String page) throws Exception {
 
@@ -32,6 +37,19 @@ public class CopybookBatchReader implements Closeable {
 		 * .getCobolIOBuilder(copybookFile.toString())
 		 * .setFileOrganization(Constants.IO_FIXED_LENGTH) .setFont("cp037");
 		 */
+		// NOTE: highly unstable"
+		LayoutDetail layoutDetail = ioBuilder.getLayout();
+		if (layoutDetail.getRecordCount() == 0) {
+			throw new IllegalStateException(String.format("Copybook %s defines no records", copybookFile.toString()));
+		}
+		recordSize = layoutDetail.getRecord(0).getLength();		
+		log.info("record size = {}", recordSize);
+		RecordDetail recordDetail = layoutDetail.getRecord(0);
+		for (FieldDetail fieldDetail : recordDetail.getFields()) {
+			String fieldName = fieldDetail.getName();
+			log.info("record field = {}", fieldName);
+		}
+		
 		reader = ioBuilder.newReader(dataFile.toString());
 	}
 
