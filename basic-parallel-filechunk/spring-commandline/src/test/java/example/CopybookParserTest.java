8ea83ebe7e00e099653f13d5aa4aa8233fc5dc9d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-03-03 23:02:30 -0500

    intermediate revision - added unit tests: positive and negative

---
+++ basic-parallel-filechunk/spring-commandline/src/test/java/example/CopybookParserTest.java
@@ -0,0 +1,73 @@
+package example;
+
+import net.sf.JRecord.Common.RecordException;
+import net.sf.JRecord.JRecordInterface1;
+import net.sf.JRecord.def.IO.builders.ICobolIOBuilder;
+// import net.sf.JRecord.External.LayoutDetail;
+import net.sf.JRecord.Details.LayoutDetail;
+import net.sf.JRecord.Details.RecordDetail;
+import net.sf.JRecord.Details.AbstractLine;
+import net.sf.JRecord.Details.fieldValue.IFieldValue;
+import net.sf.JRecord.IO.AbstractLineReader;
+import net.sf.JRecord.IO.AbstractLineWriter;
+import net.sf.JRecord.IO.LineIOProvider;
+import net.sf.JRecord.JRecordInterface1;
+import net.sf.JRecord.def.IO.builders.ICobolIOBuilder;
+import net.sf.JRecord.Common.Constants;
+import net.sf.JRecord.Common.FieldDetail;
+import net.sf.JRecord.Common.IFileStructureConstants;
+import net.sf.JRecord.Numeric.ICopybookDialects;
+
+import net.sf.JRecord.Common.Constants;
+import net.sf.JRecord.Numeric.ICopybookDialects;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.CoreMatchers.containsString;
+
+import static org.hamcrest.Matchers.is;
+
+// import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+
+class CopybookParserTest {
+
+	@DisplayName("Fails on Simple crafted Copybook With the number not88 Error")
+	@Test
+	public void test1() {
+		String copybookPath = "src/test/resources/example-dc3aa31c.cbl";
+
+		ICobolIOBuilder iCobolIOBuilder = JRecordInterface1.COBOL.newIOBuilder(copybookPath)
+				.setDialect(ICopybookDialects.FMT_MAINFRAME).setFileOrganization(Constants.IO_FIXED_LENGTH)
+				.setFont("utf8");
+
+		RecordException ex = assertThrows(RecordException.class, iCobolIOBuilder::getLayout);
+
+		// "Expected parser to throw exception"
+		assertThat(ex.getMessage(), containsString("number88"));
+	}
+
+	@DisplayName("Reads Copybook Without Error")
+	@Test
+	void test2() throws IOException {
+		String copybookPath = "src/test/resources/example.cbl";
+
+		ICobolIOBuilder iCobolIOBuilder = JRecordInterface1.COBOL.newIOBuilder(copybookPath)
+				.setDialect(ICopybookDialects.FMT_MAINFRAME).setFileOrganization(Constants.IO_FIXED_LENGTH)
+				.setFont("utf8");
+		LayoutDetail layoutDetail = assertDoesNotThrow(iCobolIOBuilder::getLayout);
+		// "Expected parser to reas layout detail"
+		assertThat(layoutDetail, notNullValue());
+	}
+}
