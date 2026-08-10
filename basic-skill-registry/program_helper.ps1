09060ef5d1dc041a18185adf6d79e2f4864538bc
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-08-10 14:59:15 -0400

    intermediate revision - added back port to powershell 2.0 / .net 4.0 / windows XP

--- basic-skill-registry/program_helper.ps1
+++ basic-skill-registry/program_helper.ps1
@@ -3,12 +3,10 @@ using System;
 using System.Diagnostics;
 using System.IO;
 
-namespace Utils
-{
+namespace Utils {
 	// NOTE:
 	// will cannot declare instance members in a static class
-	public class Program
-	{
+	public class Program {
 		private string tempPath;
 		public string TempPath {
 			get { return tempPath; }
@@ -18,14 +16,18 @@ namespace Utils
 			get { return location; }
 			set { location = value; }
 		}
-		public static string CreateTempSubdirectory(string prefix = "")
-		{
+		// .Net 4.0: Default parameter specifiers are not permitted
+		public static string CreateTempSubdirectory() {
+			return CreateTempSubdirectory("");
+		}
+
+		public static string CreateTempSubdirectory(string prefix) {
 			// Get the system temp path
 			string tempRoot = Path.GetTempPath();
-        
+
 			// Create a unique folder name (optional prefix + GUID)
-			string uniqueFolderName = string.IsNullOrEmpty(prefix) 
-            ? Guid.NewGuid().ToString() 
+			string uniqueFolderName = string.IsNullOrEmpty(prefix)
+            ? Guid.NewGuid().ToString()
             : prefix + "_" + Guid.NewGuid().ToString();
 
 			// Combine root and unique subfolder name
@@ -33,45 +35,43 @@ namespace Utils
 
 			// Create and return the physical directory
 			Directory.CreateDirectory(uniquePath);
-        
 			return uniquePath;
 		}
-    
-		public void Run()
-		{
 
+		public void Run() {
 
 			// NOTE: custom extension returning String, not DirectryInfo hence no FullPath
 			// built-in Directory.CreateTempSubdirectory() method does not exist in .NET 4.5 (it was introduced later in .NET Core 3.0 / .NET Standard 2.1).
 			// therefore the following
-			// https://learn.microsoft.com/en-us/dotnet/api/system.io.directory.createtempsubdirectory?view=net-10.0?view=netframework-4.5 
+			// https://learn.microsoft.com/en-us/dotnet/api/system.io.directory.createtempsubdirectory?view=net-10.0?view=netframework-4.5
 			//	is redirect
 			// https://learn.microsoft.com/en-us/dotnet/api/system.io.directory.createtempsubdirectory?view=net-10.0&viewFallbackFrom=net-10.0%3Fview%3Dnetframework-4.5
 			this.tempPath = CreateTempSubdirectory(Path.GetTempPath());
-			var process = new Process {
-				// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo?view=netframework-4.5
-				StartInfo = new ProcessStartInfo {
-      
-					WorkingDirectory = tempPath,
-					// NOTE: @"" does not appear to work under add-type  
-					// FileName = @"C:\Program Files\Git\bin\git.exe",
-					FileName = "C:\\Program Files\\Git\\bin\\git.exe",
-					Arguments = String.Format("clone --depth 1 \"{0}\"", this.location),
-					UseShellExecute = false,
-				}
-			};
+			// .Net 4.0  A new expression requires () or [] after type
+			// .Net 4.0 The type or namespace name 'var' could not be found
+			Process process = new Process();
+			// https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo?view=netframework-4.5
+			process.StartInfo = new ProcessStartInfo();
+
+			process.StartInfo.WorkingDirectory = tempPath;
+			// NOTE: @"" does not appear to work under add-type
+			// FileName = @"C:\Program Files\Git\bin\git.exe";
+			process.StartInfo.FileName = "C:\\Program Files\\Git\\bin\\git.exe";
+			process.StartInfo.Arguments = String.Format("clone --depth 1 \"{0}\"", this.location);
+			process.StartInfo.UseShellExecute = false;
+
 			process.Start();
 			process.WaitForExit();
 
-      try {
-        Directory.Delete(tempPath, true);
-      } catch (Exception) {
-      }
-    } 
+			try {
+				Directory.Delete(tempPath, true);
+			} catch (Exception) {
+			}
+		}
 	}
-}  
+}
 '@
-# $git_install_path = ( Get-ItemProperty -Path 'HKLM:\SOFTWARE\GitForWindows').InstallPath  
+# $git_install_path = ( Get-ItemProperty -Path 'HKLM:\SOFTWARE\GitForWindows').InstallPath
 
 # $window_handle = [System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle
 $helper = new-object Utils.Program
@@ -79,5 +79,5 @@ $helper = new-object Utils.Program
 $helper.Location = "https://github.com/membranedev/application-skills";
 $helper.Run()
 write-output $helper.TempPath
-  
+
 # Exception calling "run" with "0" argument(s): "Access to the path 'C:\Users\kouzm\AppData\Local\Temp\_e57611bf-0351-4731-916f-f082e1bd671e' is denied."
